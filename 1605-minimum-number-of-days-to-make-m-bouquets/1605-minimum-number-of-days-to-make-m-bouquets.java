c893cc61847008edb3bc5ca9aa383eea22d3829f
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-07-01 12:05:00 -0700

    Time: 22 ms (39.38%), Space: 58 MB (45.44%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,11 +1,10 @@
 class Solution {
-    public boolean checkPossibility(int[] bloomDay, int m, int k, int day) {
-        int n = bloomDay.length;
+    public boolean checkIfPossible(int[] bloomDay, int days, int m, int k, int n) {
         int bloomedFlowers = 0;
         int bouquetMade = 0;
 
         for(int i = 0; i < n; i++) {
-            if(bloomDay[i] <= day) {
+            if(bloomDay[i] <= days) {
                 bloomedFlowers++;
             } else {
                 bouquetMade += bloomedFlowers / k;
@@ -16,24 +15,22 @@ class Solution {
         return bouquetMade >= m;
     }
     public int minDays(int[] bloomDay, int m, int k) {
-        int flowers = bloomDay.length;
+        int n = bloomDay.length;
 
-        if((long)m*k > flowers) return -1;
+        if((long)m*k > n) return -1;
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < flowers; i++) {
-            min = Math.min(min, bloomDay[i]);
-            max = Math.max(max, bloomDay[i]);
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            low = Math.min(bloomDay[i], low);
+            high = Math.max(bloomDay[i], high);
         }
-
-        int low = min;
-        int high = max;
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            boolean isPossible = checkPossibility(bloomDay, m, k, mid);
+            boolean isPossible = checkIfPossible(bloomDay, mid, m, k, n);
 
             if(isPossible == true) high = mid - 1;
             else low = mid + 1;
