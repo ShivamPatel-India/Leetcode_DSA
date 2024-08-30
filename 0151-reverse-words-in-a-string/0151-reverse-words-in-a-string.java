abb8a197016e1a0d89d01a74e471b3f8c2b99166
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-08-30 15:19:17 -0700

    Time: 17 ms (10.29%), Space: 45.4 MB (11%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String reverseWords(String s) {
+        // Trim leading and trailing spaces
+        s = s.trim();
+        
+        int left = 0;
+        int right = s.length() - 1;
+
+        String temp = "";
+        String ans = "";
+
+        // Iterate the string and keep on adding to form a word
+        // If a space is encountered, add the current word to the result
+        while (left <= right) {
+            char ch = s.charAt(left);
+            if (ch != ' ') {
+                temp += ch;
+            } else if (ch == ' ') {
+                if (!temp.isEmpty()) {
+                    if (!ans.isEmpty()) {
+                        ans = temp + " " + ans;
+                    } else {
+                        ans = temp;
+                    }
+                    temp = "";
+                }
+            }
+            left++;
+        }
+
+        // If temp is not empty, add it to the result (adding the last word)
+        if (!temp.isEmpty()) {
+            if (!ans.isEmpty()) {
+                ans = temp + " " + ans;
+            } else {
+                ans = temp;
+            }
+        }
+
+        return ans;
+    }
+}
