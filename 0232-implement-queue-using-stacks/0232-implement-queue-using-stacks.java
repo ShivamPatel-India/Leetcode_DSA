5374f4b739368bd676ff6c84e5c09803cab7aad7
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-10-23 22:15:37 -0700

    Time: 0 ms (100%), Space: 41.9 MB (5.43%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,3 +1,7 @@
+/*
+
+// This is the first approach where push operation is more expensive. So this //approach can be used when dealing with so many pop or top operations.
+
 class MyQueue {
 
     // using two stacks to implement queue data structure
@@ -35,7 +39,57 @@ class MyQueue {
         return s1.isEmpty();
     }
 }
+*/
 
+
+// This is the second aaproach where we made the push operation less expensive than pop and top.
+
+class MyQueue {
+
+    // using two stacks to implement queue data structure
+    // For any push operation into queue;
+    // 1st step: stack1 -> stack2; (take all the elements from s1 to s2);
+    // 2nd step:  x --> stack1; (push the element into s1);
+    // 3rd step: transfer back everything form s2 to s1 
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        s1.push(x);
+    }
+    
+    public int pop() {
+        if(s2.isEmpty()) {
+            while(!s1.isEmpty()) s2.push(s1.pop());
+            return s2.pop();
+        } else return s2.pop();
+    }
+    
+    public int peek() {
+        if(s2.isEmpty()) {
+            while(!s1.isEmpty()) s2.push(s1.pop());
+            return s2.peek();
+        } else return s2.peek();
+    }
+    
+    public boolean empty() {
+        return ( s1.size() + s2.size() ) == 0;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
