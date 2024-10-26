915022db6c14649a76e9d946e295d7c499307334
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-10-26 15:08:53 -0700

    Time: 4 ms (95.99%), Space: 44.7 MB (71.83%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,10 +1,9 @@
-// Optimal approach
-
 class MinStack {
-    Stack<Long> st = new Stack<>();
+    Stack<Long> st;
     Long min;
 
     public MinStack() {
+        st = new Stack<>();
         min = Long.MAX_VALUE;
     }
     
@@ -14,13 +13,11 @@ class MinStack {
         if(st.isEmpty()) {
             st.push(val);
             min = val;
+        } else if (val < min) {
+            st.push(2 * val - min);
+            min = val;
         } else {
-            if(val < min) {
-                st.push(2*val - min);
-                min = val;
-            } else {
-                st.push(val);
-            }
+            st.push(val);
         }
     }
     
@@ -28,64 +25,26 @@ class MinStack {
         if(st.isEmpty()) return;
 
         Long val = st.pop();
-
         if(val < min) {
             min = 2 * min - val;
         }
     }
     
     public int top() {
-        if(st.isEmpty()) return -1;
+        if(st.isEmpty()) {
+            return -1;
+        } 
 
         Long val = st.peek();
-        if(val < min) return min.intValue();
-        else return val.intValue();
+        if(val < min) {
+            return min.intValue();
+        } else return val.intValue();
     }
     
     public int getMin() {
         return min.intValue();
     }
 }
-
-/* The solution given below requires an extra space of O(2N) bcz we are using Pairs.
-
-class Pair {
-    int x;
-    int y;
-    Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class MinStack {
-
-    Stack<Pair> st;
-
-    public MinStack() {
-        st = new Stack<>();
-    }
-    
-    public void push(int val) {
-        int min;
-        if(st.isEmpty()) min = val;
-        else min = Math.min(val, st.peek().y);
-        st.push(new Pair(val, min));
-    }
-    
-    public void pop() {
-        st.pop();
-    }
-    
-    public int top() {
-        return st.peek().x;
-    }
-    
-    public int getMin() {
-        return st.peek().y;
-    }
-}
-*/
 
 /**
  * Your MinStack object will be instantiated and called as such:
