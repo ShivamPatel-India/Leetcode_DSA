09b33040caf2efc1ddc27b654598fd28de56c038
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-10-27 02:42:07 -0700

    Time: 3 ms (92.54%), Space: 43.9 MB (37.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,24 +5,11 @@ class Solution {
         Map<Integer, Integer> nge = new HashMap<Integer, Integer>();
 
         for(int i = n-1; i >= 0; i--) {
-            if(st.isEmpty()) {
-                nge.put(nums2[i],-1);
-                st.push(nums2[i]);
-            }
-            else if (st.peek() > nums2[i]) {
-                nge.put(nums2[i],st.peek());
-                st.push(nums2[i]);
-            } else {
-                while(!st.isEmpty() && st.peek() < nums2[i]) st.pop();
-                if(st.isEmpty()) {
-                    nge.put(nums2[i],-1);
-                    st.push(nums2[i]);
-                }
-                else if(st.peek() > nums2[i]) {
-                    nge.put(nums2[i],st.peek());
-                    st.push(nums2[i]);
-                }
-            }
+            while(!st.isEmpty() && st.peek() <= nums2[i]) st.pop();
+
+            if(st.isEmpty()) nge.put(nums2[i], -1);
+            else nge.put(nums2[i], st.peek());
+            st.push(nums2[i]);
         }
         int k = nums1.length; 
         int[] ans = new int[k];
