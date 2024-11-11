0fe489b34d1d5b2c41621d6274c58bf151777d2b
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-11-11 14:19:24 -0800

    Time: 99 ms (7.71%), Space: 45.8 MB (26.11%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -3,28 +3,26 @@ class Solution {
         Stack<Character> st = new Stack<>();
 
         for(char c : num.toCharArray()) {
-            while(!st.isEmpty() && k > 0 && st.peek() - '0' > c - '0') {
+            while(!st.isEmpty() && k > 0 && (st.peek() - '0') > (c - '0')) {
                 st.pop();
                 k--;
             }
             st.push(c);
         }
-        while(k>0) {
+
+        while(k > 0) {
             st.pop();
             k--;
         }
         if(st.isEmpty()) return "0";
-
         String res = "";
         while(!st.isEmpty()) {
             res = res + st.peek();
             st.pop();
         }
-
-        while(!res.isEmpty() && res.charAt(res.length() - 1) == '0'){
-            res = res.substring(0, res.length()-1);
+        while(!res.isEmpty() && res.charAt(res.length()-1) == '0') {
+            res = res.substring(0,res.length()-1);
         }
-
         res = new StringBuilder(res).reverse().toString();
         if(res.isEmpty()) return "0";
         return res;
