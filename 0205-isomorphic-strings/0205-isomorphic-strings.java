8abc8295c2e3d9faaf22d54709c6d2fc43434ca2
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-11-13 22:08:00 -0800

    Time: 11 ms (70.1%), Space: 42 MB (93.09%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -6,13 +6,19 @@ class Solution {
         for(int i = 0; i < s.length(); i++) {
             char sch = s.charAt(i);
             char tch = t.charAt(i);
+            // checking if the key is in the map or not
             if(!mp.containsKey(sch)) {
+                // if the key is not in the map then the value associated with it that is tch also should not be in the map, and if its there then strings are not isomorphi hence returning false
                 if(mp.containsValue(tch)) return false;
+                // otherwise making the entry in map
                 mp.put(sch, tch);
             } else {
+                // if key is there in the map, then comparing its value with currenct tch
                 if(tch != mp.get(sch)) return false;
             }
         }
+
+        // if none of the false condition executed in the above block, just return true
         return true;
     }
 }
