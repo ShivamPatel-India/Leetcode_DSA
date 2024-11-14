7a046bf1fefe1dbab1ef78ac72a313bc1346e4f3
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-11-14 14:07:23 -0800

    Time: 0 ms (100%), Space: 41 MB (89.36%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // when we concatenate the string with itself, it will contains all the possible combination
         if(s.length() != goal.length()) return false;
         return (s+s).contains(goal);
     }
