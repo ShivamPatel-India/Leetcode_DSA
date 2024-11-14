3d88502bf8ba8378eda99d1fd797f7a949be8597
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-11-14 15:54:46 -0800

    Time: 3 ms (90.3%), Space: 43.9 MB (84.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,29 +2,24 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
 
-        // using array based hashset cause having fixed numebr of keys (ascii chars in this case)
-        int hash[] = new int[256];
-        Arrays.fill(hash, -1); 
+        // using array based hashing because we have fixed number of keys (ascii characters in this case)
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
 
         int l = 0, r = 0, len = 0, maxlen = 0;
 
         while(r<n) {
-            // if current char is in the map
+            // if entry for the character is already there in the hashmap
             if(hash[(int)s.charAt(r)] != -1) {
-                // update the value of l if the char is encountered after it
-                if(l <= hash[(int)s.charAt(r)]) {
+                if(hash[(int)s.charAt(r)] >= l) {
                     l = hash[(int)s.charAt(r)] + 1;
                 }
             }
-            // calculate the new length
-            len = r-l+1;
-            // update the maxlen
-            maxlen = Math.max(maxlen, len);
-            // make the entry of r in the hash array
+            len = r - l + 1;
+            maxlen = Math.max(len, maxlen);
             hash[(int)s.charAt(r)] = r;
             r++;
         }
-        // return the maxlen counted after the full traversal
         return maxlen;
     }
 }
