a6915ea7989b499333edc70d0dcb8ca0ee1d32ff
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-11-15 23:24:55 -0800

    Time: 871 ms (5.03%), Space: 42.4 MB (66.28%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,17 +1,16 @@
-// Brute force : generate all the substring
 class Solution {
     public int characterReplacement(String s, int k) {
+        int maxlen = 0;
         int hash[] = new int[26];
+        int changes = 0;
 
-        int changes = 0, maxlen = 0;
-        for(int i = 0; i< s.length(); i++) {
+        for(int i = 0; i<s.length(); i++) {
             Arrays.fill(hash, 0);
             int maxFreq = 0;
             for(int j = i; j<s.length(); j++) {
-                hash[s.charAt(j)-'A']++;
-                maxFreq = Math.max(maxFreq, hash[s.charAt(j)-'A']);
-                changes = (j - i + 1) - maxFreq;
-                if(changes <= k) maxlen = Math.max(maxlen, (j-i+1));
+                maxFreq = Math.max(maxFreq, ++hash[s.charAt(j)-'A']);
+                changes = (j-i+1)-maxFreq;
+                if(changes <= k) maxlen = Math.max(maxlen, j-i+1);
                 else break;
             }
         }
