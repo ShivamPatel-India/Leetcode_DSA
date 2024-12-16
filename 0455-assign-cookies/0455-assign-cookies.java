5b206fe8e3037686d48a39334f94b1c4f08fd1a6
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-12-16 20:34:35 +0530

    Time: 10 ms (83.51%), Space: 48.4 MB (69.48%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,32 +1,17 @@
-// Time complexity: O(g log g + c log c + c)
-// g log g to sort the greed array 
-// c log c to sort the cookies array
-// c because we are traversing the cookies array and at most all the cookies can be given to satisfy the greed
 class Solution {
-    public int findContentChildren(int[] greed, int[] cookies) {
-        // length of the greed array
-        int greedLength = greed.length;
-        // pointer for greed array
+    public int findContentChildren(int[] g, int[] s) {
         int gp = 0;
-        // length of the cookies array
-        int cookiesLength = cookies.length;
-        // pointer for cookies array
-        int cp = 0;
-        
-        // sort the greed and cookies array in ascending order
-        Arrays.sort(greed);
-        Arrays.sort(cookies);
+        int sp = 0;
 
-        // traversing the array 
-        while(gp < greedLength && cp < cookiesLength) {
-            // if the greed can be satisfied than increase the greed pointer
-            if(greed[gp] <= cookies[cp]) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(gp < g.length && sp < s.length) {
+            if(g[gp] <= s[sp]){
                 gp++;
             }
-            // increase the cookies pointer in either of the cases
-            cp++;
+            sp++;
         }
-        // returning the greed pointer reffering to the total greed satisfied
         return gp;
     }
 }
