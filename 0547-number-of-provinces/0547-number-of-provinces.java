4cb3d758780b1aa99772676cd91a3431c0024e68
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-12-27 21:19:25 +0530

    Time: 0 ms (100%), Space: 48 MB (36.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,22 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length; // Number of nodes (cities)
-        boolean[] visited = new boolean[n]; // Track visited nodes
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
         int provinces = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                // Start a new DFS if the node hasn't been visited
-                dfs(isConnected, visited, i);
-                provinces++; // Each DFS call corresponds to a new province
+        for(int i = 0; i<n; i++) {
+            if(!vis[i]) {
+                provinces++;
+                dfs(i, isConnected, vis);
             }
         }
         return provinces;
     }
-
-    private void dfs(int[][] isConnected, boolean[] visited, int currentNode) {
-        visited[currentNode] = true; // Mark the current node as visited
-        for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
-            // If there's a connection and the neighbor hasn't been visited, explore it
-            if (isConnected[currentNode][neighbor] == 1 && !visited[neighbor]) {
-                dfs(isConnected, visited, neighbor);
+    static void dfs(int currentNode, int[][] isConnected, boolean[] vis) {
+        vis[currentNode] = true;
+        for(int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+            if(isConnected[currentNode][neighbor] == 1 && !vis[neighbor]) {
+                dfs(neighbor, isConnected, vis);
             }
         }
     }
