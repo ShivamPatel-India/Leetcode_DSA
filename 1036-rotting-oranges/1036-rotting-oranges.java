26ec02cc642229f25b078b55eb4e25c3a3a01f75
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2024-12-28 15:15:28 +0530

    Time: 4 ms (8.79%), Space: 42 MB (90.48%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,48 +1,55 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        // get the dimensions
         int rows = grid.length;
         int cols = grid[0].length;
+
+        // Queue to keep track of the coordinates
         Queue<int[]> queue = new LinkedList<>();
 
-        // put the position of all rotten oranges in queue and count the fresh oranges
-        int count_fresh = 0;
-        for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < cols ; j++) {
-                if(grid[i][j] == 2) {
-                    queue.offer(new int[]{i , j});
-                }
-                if(grid[i][j] != 0) {
-                    count_fresh++;
-                }
+        int total_oranges = 0; // fresh + rotten oranges
+
+        // count the total oranges and put the rotten oranges in the queue
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if(grid[i][j] != 0) total_oranges++;
+                if(grid[i][j] == 2) queue.offer(new int[]{i,j});
             }
         }
-       
-        if(count_fresh == 0) return 0;
-        int countMin = 0, cnt = 0;
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
-        
-        //bfs starting from initially rotten oranges
+
+        if(total_oranges == 0) return 0;
+
+        // to keep track of the time taken to rot all oranges
+        int minTime = 0;
+        // to keep track of how many oranges we have explored or has been rotten
+        int exploredOranges = 0;
+
+        // directions: down, up, right, left
+        int dx[] = {0,0,1,-1}; // right, left
+        int dy[] = {1,-1,0,0}; // down, up
+
+        // now take the rotten oranges from the queue and run the BFS algorithm to rot its adjacent oranges and add them to the queue so that those can be used int further levels of BFS to rot their respective adjacent oranges
         while(!queue.isEmpty()) {
             int size = queue.size();
-            cnt += size; 
-            for(int i = 0 ; i < size ; i++) {
-                int[] point = queue.poll();
-                for(int j = 0;j<4;j++) {
+            exploredOranges += size;
+            for(int i = 0; i < size; i++) {
+                int point[] = queue.poll();
+                for(int j = 0; j < 4; j++) {
                     int x = point[0] + dx[j];
                     int y = point[1] + dy[j];
-                    
-                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || 
-                    grid[x][y] == 2) continue;
+
+                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)
+                        continue;
                     
                     grid[x][y] = 2;
-                    queue.offer(new int[]{x , y});
+
+                    queue.offer(new int[]{x,y});
                 }
             }
             if(queue.size() != 0) {
-                countMin++;
+                minTime++;
             }
         }
-        return count_fresh == cnt ? countMin : -1;
+        return total_oranges == exploredOranges ? minTime : -1;
     }
 }
