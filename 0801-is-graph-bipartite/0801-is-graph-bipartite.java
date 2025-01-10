f1d9b8ba69c20fcf30e7b266c6ed820dfc2bd667
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2025-01-10 11:08:57 +0530

    Time: 0 ms (100%), Space: 44.4 MB (99.52%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -5,13 +5,11 @@ class Solution {
         for(int i = 0; i < graph[node].length; i++) {
             int v = graph[node][i];
             if(color[v] == -1) {
-                if(dfs(v,1-clr,graph,color)==false) return false;
-            }
-            else if(color[v] == clr) return false;
+                if(!dfs(v,1-clr,graph,color)) return false;
+            } else if (color[v] == clr) return false;
         }
         return true;
     }
-
     public boolean isBipartite(int[][] graph) {
         int V = graph.length;
         int[] color = new int[V];
@@ -20,11 +18,9 @@ class Solution {
             color[i] = -1;
         }
 
-        // for connencted components
         for(int i = 0; i < V; i++) {
             if(color[i] == -1) {
-                // our colors are 0 and 1, and start coloring by 0
-                if(dfs(i,0,graph,color) == false) return false;
+                if(!dfs(i,0,graph, color)) return false;
             }
         }
         return true;
