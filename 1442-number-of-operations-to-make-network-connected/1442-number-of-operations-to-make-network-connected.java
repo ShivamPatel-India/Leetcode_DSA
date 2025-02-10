1f91b66ea2560703e285f6575d9f0384ffad8ded
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2025-02-10 14:20:16 -0800

    Time: 17 ms (30.49%), Space: 61.6 MB (52.31%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -2,7 +2,7 @@ class DisjointSet {
     List<Integer> parent = new ArrayList<>();
     List<Integer> rank = new ArrayList<>();
     DisjointSet(int n) {
-        for(int i = 0; i <= n; i++) {
+        for(int i = 0 ; i <= n; i++) {
             parent.add(i);
             rank.add(0);
         }
@@ -12,20 +12,19 @@ class DisjointSet {
         if(node == parent.get(node)) return node;
         else {
             int ulp = findUPar(parent.get(node));
-            parent.set(node,ulp);
+            parent.set(node, ulp);
             return parent.get(node);
         }
     }
-
     public void unionByRank(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
 
         if(ulp_u == ulp_v) return;
-        
-        if(rank.get(ulp_v) < rank.get(ulp_u)) {
+
+        if(rank.get(ulp_u) > rank.get(ulp_v)) {
             parent.set(ulp_v, ulp_u);
-        } else if(rank.get(ulp_u) < rank.get(ulp_v)) {
+        } else if (rank.get(ulp_v) > rank.get(ulp_u)) {
             parent.set(ulp_u, ulp_v);
         } else {
             parent.set(ulp_v, ulp_u);
@@ -38,20 +37,20 @@ class Solution {
     public int makeConnected(int n, int[][] connections) {
         DisjointSet ds = new DisjointSet(n);
         int extraEdges = 0;
-        int edges = connections.length;
-        for(int i = 0; i < edges; i++) {
+        int m = connections.length;
+        for(int i = 0; i < m; i++) {
             int u = connections[i][0];
             int v = connections[i][1];
             if(ds.findUPar(u) == ds.findUPar(v)) extraEdges++;
-            else ds.unionByRank(u, v);
+            else ds.unionByRank(u,v);
         }
-
         int cntC = 0;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++) {
             if(ds.parent.get(i) == i) cntC++;
         }
         int ans = cntC - 1;
         if(extraEdges >= ans) return ans;
-        return -1;
+        else return -1;
+
     }
 }
