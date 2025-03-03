ad546f08ac93cdd938a28cfcc8fe954cd11b0141
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2025-03-03 13:30:15 -0800

    Time: 2 ms (70.14%), Space: 42.3 MB (85.34%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -4,52 +4,51 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
 
-        // Queue to keep track of the coordinates
-        Queue<int[]> queue = new LinkedList<>();
+        // queue to keep track of the co-ordinates
+        Queue<int[]> q  = new LinkedList<>();
 
-        int total_oranges = 0; // fresh + rotten oranges
+        int total_oranges = 0;
 
         // count the total oranges and put the rotten oranges in the queue
-        for(int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for(int i = 0 ; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
                 if(grid[i][j] != 0) total_oranges++;
-                if(grid[i][j] == 2) queue.offer(new int[]{i,j});
+                if(grid[i][j] == 2) q.offer(new int[]{i,j});
             }
         }
 
         if(total_oranges == 0) return 0;
 
-        // to keep track of the time taken to rot all oranges
         int minTime = 0;
-        // to keep track of how many oranges we have explored or has been rotten
         int exploredOranges = 0;
 
-        // directions: down, up, right, left
-        int dx[] = {0,0,1,-1}; // right, left
-        int dy[] = {1,-1,0,0}; // down, up
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
 
-        // now take the rotten oranges from the queue and run the BFS algorithm to rot its adjacent oranges and add them to the queue so that those can be used int further levels of BFS to rot their respective adjacent oranges
-        while(!queue.isEmpty()) {
-            int size = queue.size();
+        while(!q.isEmpty()) {
+            int size = q.size();
             exploredOranges += size;
-            for(int i = 0; i < size; i++) {
-                int point[] = queue.poll();
+
+            for(int i = 0 ; i < size; i++) {
+                int[] point = q.poll();
                 for(int j = 0; j < 4; j++) {
                     int x = point[0] + dx[j];
                     int y = point[1] + dy[j];
 
-                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)
+                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] ==2) 
                         continue;
                     
                     grid[x][y] = 2;
-
-                    queue.offer(new int[]{x,y});
+                    // making th eentry of newly rotten orange in the queue so that it can rott its neighbour
+                    q.offer(new int[]{x,y});
                 }
             }
-            if(queue.size() != 0) {
+            if(q.size() != 0) {
                 minTime++;
             }
         }
+
         return total_oranges == exploredOranges ? minTime : -1;
+
     }
 }
