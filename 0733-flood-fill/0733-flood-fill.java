e54906956504e25f9acf50b951589602cda18ac5
ShivamPatel <70719016+ShivamPatel-India@users.noreply.github.com>
2025-03-06 22:34:30 -0800

    Time: 0 ms (100%), Space: 44.9 MB (66.12%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,32 +1,24 @@
 class Solution {
-    public void dfs(int row, int col, int[][] image, int[][] ans, int[] dx, int[] dy,int color, int initialColor) {
-        ans[row][col] = color;
-        int rowCount = image.length;
-        int colCount = image[0].length;
-        for(int  i = 0; i < 4; i++) {
-            int nrow = row + dx[i];
-            int ncol = col + dy[i];
+    public void dfs(int sr,int sc,int[][] image,int color,int initialColor,int[] dx,int[] dy,int[][] ans) {
+        ans[sr][sc] = color;
+        int rows = image.length;
+        int cols = image[0].length;
+        for(int i = 0; i < 4; i++) {
+            int nrow = sr + dx[i];
+            int ncol = sc + dy[i];
 
-            if(nrow>=0 && nrow<rowCount && ncol>=0 && ncol<colCount && ans[nrow][ncol]!=color &&
-            image[nrow][ncol]==initialColor) {
-                dfs(nrow, ncol, image, ans, dx, dy, color, initialColor);
+            if(nrow>=0 && ncol>=0 && nrow<rows && ncol<cols && ans[nrow][ncol]!=color && image[nrow][ncol]==initialColor) {
+                dfs(nrow,ncol,image,color,initialColor,dx,dy,ans);
             }
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // get the initial color
         int initialColor = image[sr][sc];
-
-        // ans array to return the final output
         int[][] ans = image;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
 
-        // arrays defining the changes in direction coordinates
-        int[] dx = {0,0,1,-1};
-        int[] dy = {1,-1,0,0};
-
-        // start the dfs recursion
-        dfs(sr,sc,image,ans,dx,dy,color,initialColor);
-
+        dfs(sr,sc,image,color,initialColor,dx,dy,ans);
         return ans;
     }
 }
