9d9b439b39d36beb657a661c3d8c0945808a6a84
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-19 12:44:00 -0700

    Time: 5 ms (17.8%), Space: 43.2 MB (71.7%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public static void dfs(int[] nums, List<List<Integer>> ans, List<Integer> cur, int i, int n, int target, int total) {
         if(total == target) {
-            ans.add(new ArrayList<>(cur));
+            ans.add(new ArrayList(cur));
             return;
         }
-        if(total > target || i == n) return;
+        if(i >= n || total > target) return;
 
         cur.add(nums[i]);
-        dfs(nums, ans, cur, i + 1, n, target, total + nums[i]);
+        dfs(nums, ans, cur, i+1, n, target, total + nums[i]);
         cur.removeLast();
 
-        while(i+1 < n && nums[i] == nums[i+1]) i++;
-        dfs(nums, ans, cur, i + 1, n, target, total);
+        while(i+1<n && nums[i] == nums[i+1]) i++;
+        dfs(nums, ans, cur, i+1, n, target, total);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
