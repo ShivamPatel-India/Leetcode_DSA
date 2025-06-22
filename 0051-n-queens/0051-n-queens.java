3eeb2480d6fc5e5ac10fe450d85cddac34bb64a4
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-21 20:44:09 -0700

    Time: 1 ms (99.77%), Space: 45 MB (56.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 board[i][j] = '.';
             }
@@ -19,25 +19,25 @@ class Solution {
             return;
         }
 
-        for(int row = 0; row < board.length; row++) {
-            if(leftRow[row] == 0 && lowerDiag[row + col] == 0 && upperDiag[board.length - 1 + col - row] == 0) {
+        for(int row = 0 ; row < board.length; row++) {
+            if(leftRow[row] == 0 && lowerDiag[row + col] == 0 && upperDiag[board.length-1+col-row] == 0) {
                 board[row][col] = 'Q';
                 leftRow[row] = 1;
-                lowerDiag[row + col] = 1;
-                upperDiag[board.length - 1 + col - row] = 1;
-                solve(col + 1, board, res, leftRow, upperDiag, lowerDiag);
+                lowerDiag[row+col] = 1;
+                upperDiag[board.length-1+col-row] =1;
+                solve(col+1, board, res, leftRow, upperDiag, lowerDiag);
                 board[row][col] = '.';
                 leftRow[row] = 0;
-                lowerDiag[row + col] = 0;
-                upperDiag[board.length - 1 + col - row] = 0;
+                lowerDiag[row+col] = 0;
+                upperDiag[board.length-1+col-row] = 0;
             }
         }
     }
     private List<String> construct(char[][] board) {
-        List<String> res = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
         for(int i = 0; i < board.length; i++) {
-            res.add(new String(board[i]));
+            ans.add(new String(board[i]));
         }
-        return res;
+        return ans;
     }
 }
