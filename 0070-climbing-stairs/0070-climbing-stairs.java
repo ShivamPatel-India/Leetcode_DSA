c01643049021cd34be988c84f256ba59edf7d134
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-22 23:47:52 -0700

    Time: 0 ms (100%), Space: 40.2 MB (91.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -16,7 +16,7 @@ class Solution {
     public int tabulation(int n, int[] dp) {
         // bottom-up approach
         dp[0] = 1;
-        if(n >= 1) dp[1] = dp[0];
+        dp[1] = dp[0];
         for(int i = 2; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
