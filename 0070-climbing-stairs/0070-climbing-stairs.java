49a40f0c463c263912d940c157d1aae9ae17a69b
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-22 23:47:02 -0700

    Time: 0 ms (100%), Space: 40.7 MB (18.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,14 +2,25 @@ import java.util.*;
 
 class Solution {
     public int recursive(int n) {
+        // top-down approach
         if(n == 0 || n == 1) return 1;
         return recursive(n-1) + recursive(n-2);
     }
     public int memoized(int n, int[] dp) {
+        // top-down approach with memoization
         if(n == 0 || n == 1) return 1;
         if(dp[n] != -1) return dp[n];
 
         return dp[n] = memoized(n-1, dp) + memoized(n-2, dp);
+    }
+    public int tabulation(int n, int[] dp) {
+        // bottom-up approach
+        dp[0] = 1;
+        if(n >= 1) dp[1] = dp[0];
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
     public int spaceOptimized(int n) {
         int prev = 1; // 1 way to get to the 0th stair
@@ -26,8 +37,8 @@ class Solution {
         // return recursive(n); // recursive approach will give TLE
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-
-        return memoized(n, dp);
+        // return memoized(n, dp);
+        return tabulation(n, dp);
         // return spaceOptimized(n);
     }
 }
