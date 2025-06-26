ce14b7d1ac14e244870709bfbd8af0cc8184e7b0
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-26 01:20:34 -0700

    Time: 1 ms (99.75%), Space: 47.4 MB (89.49%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int recursion(int[][] grid, int i, int j) {
+        // recursive approach might give TLE
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return Integer.MAX_VALUE;
+        else {
+            int up = Integer.MAX_VALUE;
+            int left = Integer.MAX_VALUE;
+            if(i > 0) up = grid[i][j] + recursion(grid, i-1, j);
+            if(j > 0) left = grid[i][j] + recursion(grid, i, j-1);
+            return Math.min(up, left);
+        }
+    }
+
+    public int memoization(int [][] grid, int [][] dp, int i, int j) {
+        if(i == 0 && j == 0) return grid[0][0];
+        if(i < 0 || j < 0) return Integer.MAX_VALUE;        
+        if(dp[i][j] != -1) return dp[i][j];
+        else {
+            int up = Integer.MAX_VALUE;
+            int left = Integer.MAX_VALUE;
+            if(i > 0) up = grid[i][j] + memoization(grid, dp, i-1, j);
+            if(j > 0) left = grid[i][j] + memoization(grid, dp, i, j-1);
+            dp[i][j] = Math.min(up, left);
+        }
+        return dp[i][j];
+    }
+    public int minPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return memoization(grid, dp, grid.length-1, grid[0].length-1);
+    }
+}
