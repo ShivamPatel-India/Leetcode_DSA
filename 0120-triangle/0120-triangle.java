d285d187baab80645a89762506e84b32d853a2f3
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-28 00:14:40 -0700

    Time: 1 ms (99.72%), Space: 44.2 MB (53.34%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,12 +1,14 @@
 import java.util.*;
 class Solution {
     public int recursive(List<List<Integer>> t, int n, int i, int j) {
+        // TC - (2 ^ (1 + 2 + 3 + ..... + n)), SC - O(N x N)
         if(i == n-1) return t.get(n-1).get(j);
         int down = t.get(i).get(j) + recursive(t, n, i+1, j);
         int diag = t.get(i).get(j) + recursive(t, n, i+1, j+1);
         return Math.min(down, diag);
     }
     public int memoized(List<List<Integer>> t, int n, int i, int j, int[][] dp) {
+        // TC - O(N x N), SC - O(N) + O(N x N)
         if(i == n-1) return t.get(n-1).get(j);
         if(dp[i][j] != -1) return dp[i][j];
         int down = t.get(i).get(j) + memoized(t, n, i+1, j, dp);
@@ -14,6 +16,7 @@ class Solution {
         return dp[i][j] = Math.min(down, diag);
     }
     public int tabulation(List<List<Integer>> t, int n) {
+        // TC - O(N x N), SC - O(N x N)
         int[][] dp = new int[n][n];
         for(int j = 0; j < n; j++) {
             dp[n-1][j] = t.get(n-1).get(j);
@@ -28,6 +31,7 @@ class Solution {
         return dp[0][0];
     }
     public int spaceOptimized(List<List<Integer>> t, int n) {
+        // TC - O(N x N), SC - O(N)
         int[] front = new int[n];
         for(int j = 0; j < n; j++) front[j] = t.get(n-1).get(j);
         for(int i = n-2; i >= 0; i--) {
@@ -49,8 +53,8 @@ class Solution {
             Arrays.fill(dp[i],-1);
         }
         // return recursive(triangle, n, 0, 0);
-        // return memoized(triangle, n, 0, 0, dp);
+        return memoized(triangle, n, 0, 0, dp);
         // return tabulation(triangle, n);
-        return spaceOptimized(triangle, n);
+        // return spaceOptimized(triangle, n);
     }
 }
