dcc4a4381bf94221b9698aea1699ec3a635c4d47
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-29 01:11:24 -0700

    Time: 48 ms (50.51%), Space: 45.5 MB (67.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -18,12 +18,33 @@ class Solution {
         }
         return dp[n-1][k];
     }
+    public boolean spaceOptimized(int[] nums, int k, int n) {
+        boolean[] prev = new boolean[k+1];
+
+        prev[0] = true;
+        if(k >= nums[0]) prev[nums[0]] = true;
+
+        for(int ind = 1; ind < n; ind++) {
+            boolean[] cur = new boolean[k+1];
+            cur[0] = true;
+            for(int target = 1; target <= k; target++) {
+                boolean notTake = prev[target];
+
+                boolean take = false;
+                if(target >= nums[ind]) take = prev[target - nums[ind]];
+
+                cur[target] = take || notTake;
+            }
+            prev = cur;
+        }
+        return prev[k];
+    }
     public boolean canPartition(int[] nums) {
         int n = nums.length;
 
         int sum = 0;
         for(int num: nums) sum += num;
         if(sum %2 != 0) return false;
-        else return subsetSumEqualK(nums, (int)sum/2, n);
+        else return spaceOptimized(nums, (int)sum/2, n);
     }
 }
