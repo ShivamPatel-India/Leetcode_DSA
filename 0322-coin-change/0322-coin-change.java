db0f0227d4e96cfc61492f0f5b3a7c5b7e5c3a31
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-30 01:36:35 -0700

    Time: 108 ms (6.96%), Space: 45.6 MB (12.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -14,12 +14,34 @@ class Solution {
         if(coins[index] <= amount) take = 1 + memoized(coins, amount-coins[index], dp, index);
         return dp[index][amount] = Math.min(take, notTake);
     }
-    public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] row: dp) {
-            Arrays.fill(row, -1);
+
+    public int tabulation(int[] coins, int k) {
+        int n = coins.length;
+        int[][] dp = new int[n][k+1];
+
+        for(int t = 1; t <= k; t++) {
+            if(t % coins[0] == 0) dp[0][t] = (int)t/coins[0];
+            else dp[0][t] = (int) Math.pow(10, 9);
         }
-        int ans = memoized(coins, amount, dp, coins.length-1);
+
+        for(int i = 1; i < n; i++) {
+            for(int t = 1; t <= k; t++) {
+                int notTake = 0 + dp[i-1][t];
+
+                int take = (int)Math.pow(10, 9);
+                if(coins[i] <= t) take = 1 + dp[i][t-coins[i]];
+                dp[i][t] = Math.min(take, notTake);
+            }
+        }
+        return dp[n-1][k];
+    }
+    public int coinChange(int[] coins, int amount) {
+        // int[][] dp = new int[coins.length][amount+1];
+        // for(int[] row: dp) {
+        //     Arrays.fill(row, -1);
+        // }
+        // int ans = memoized(coins, amount, dp, coins.length-1);
+        int ans = tabulation(coins, amount);
         if(ans >= (int)Math.pow(10,9)) return -1;
         return ans;
     }
