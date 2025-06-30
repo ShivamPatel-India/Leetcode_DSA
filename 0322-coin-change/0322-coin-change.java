c1cf233bd0b42823f8178121355c6a1fdba4a353
ShivamPatel <shivampatel.sp515@gmail.com>
2025-06-30 01:57:48 -0700

    Time: 123 ms (5.88%), Space: 44.8 MB (47.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -35,13 +35,37 @@ class Solution {
         }
         return dp[n-1][k];
     }
+    public int spaceOptimized(int[] coins, int k) {
+        int n = coins.length;
+        int[] prev = new int[k+1];
+        
+        for(int t = 1; t <= k; t++) {
+            if(t % coins[0] == 0) prev[t] = (int)t/coins[0];
+            else prev[t] = (int)Math.pow(10,9);
+        }
+
+        for(int i = 0; i < n; i++) {
+            int[] cur = new int[k+1];
+            for(int t = 1; t <= k; t++) {
+                int notTake = 0 + prev[t];
+
+                int take = (int)Math.pow(10, 9);
+                if(coins[i] <= t) take = 1 + cur[t-coins[i]];
+
+                cur[t] = Math.min(take, notTake);
+            }
+            prev = cur;
+        }
+        return prev[k];
+    }
     public int coinChange(int[] coins, int amount) {
         // int[][] dp = new int[coins.length][amount+1];
         // for(int[] row: dp) {
         //     Arrays.fill(row, -1);
         // }
         // int ans = memoized(coins, amount, dp, coins.length-1);
-        int ans = tabulation(coins, amount);
+        // int ans = tabulation(coins, amount);
+        int ans = spaceOptimized(coins, amount);
         if(ans >= (int)Math.pow(10,9)) return -1;
         return ans;
     }
