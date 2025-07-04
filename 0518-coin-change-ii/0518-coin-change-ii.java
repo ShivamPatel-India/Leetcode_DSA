d67dd64215ac81d86ee793201a1704637220cd24
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-04 11:48:47 -0700

    Time: 9 ms (61.6%), Space: 49.4 MB (31.1%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -13,9 +13,28 @@ class Solution {
         if(coins[i] <= amount) take = memoized(amount-coins[i], coins, dp, i);
         return dp[i][amount] = take + notTake;
     }
+    public int tabulation(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+
+        for(int t = 0; t <= amount; t++) {
+            dp[0][t] = (t % coins[0] == 0) ? 1 : 0;
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int t = 0; t <= amount; t++) {
+                int notTake = dp[i-1][t];
+                int take = 0;
+                if(coins[i] <= t) take = dp[i][t-coins[i]];
+                dp[i][t] = take + notTake;
+            }
+        }
+        return dp[n-1][amount];
+    }
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length][amount+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return memoized(amount, coins, dp, coins.length-1);
+        // int[][] dp = new int[coins.length][amount+1];
+        // for(int[] row: dp) Arrays.fill(row, -1);
+        // return memoized(amount, coins, dp, coins.length-1);
+        return tabulation(coins, amount);
     }
 }
