64b3ec10375b8c062c72e67541c7ef351ef863eb
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-12 03:54:41 -0700

    Time: 5 ms (59.76%), Space: 45 MB (77.61%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -13,12 +13,34 @@ class Solution {
         }
         
     }
+    public int tabulation(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
     public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n][m];
-        for(int[] row: dp)
-            Arrays.fill(row, -1);
-        return memoize(word1, n-1, word2, m-1, dp);
+        // int n = word1.length();
+        // int m = word2.length();
+        // int[][] dp = new int[n][m];
+        // for(int[] row: dp)
+        //     Arrays.fill(row, -1);
+        // return memoize(word1, n-1, word2, m-1, dp);
+
+        return tabulation(word1, word2);
     }
 }
