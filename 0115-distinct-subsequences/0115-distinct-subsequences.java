c371f2454ed5a4588a902fbff19fab40cb812ccf
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-11 20:14:55 -0700

    Time: 18 ms (79.95%), Space: 51.4 MB (30.61%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -10,12 +10,32 @@ class Solution {
         }
         return dp[i][j] = ds(s, t, dp, i-1, j);
     }
-    public int numDistinct(String s, String t) {
+    public int tabulation(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
 
-        return ds(s, t, dp, n, m);
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public int numDistinct(String s, String t) {
+        // int n = s.length();
+        // int m = t.length();
+        // int[][] dp = new int[n+1][m+1];
+        // for(int[] row: dp) Arrays.fill(row, -1);
+        // return ds(s, t, dp, n, m);
+
+        return tabulation(s, t);
     }
 }
