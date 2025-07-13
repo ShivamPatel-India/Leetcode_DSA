489628c49891918ea33687e5017e5f9a05de48c9
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-13 01:48:01 -0700

    Time: 22 ms (39.3%), Space: 45.3 MB (40.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -43,11 +43,12 @@ class Solution {
         int m = s.length();
 
         int[] prev = new int[m+1];
-        int[] cur = new int[m+1];
+        
 
         prev[0] = 1;
         
         for(int i = 1; i <= n; i++) {
+            int[] cur = new int[m+1];
             cur[0] = isAllStars(p, i);
             for(int j = 1; j <= m; j++) {
                 if(p.charAt(i-1) == s.charAt(j-1) || p.charAt(i-1) == '?')
@@ -57,7 +58,7 @@ class Solution {
                 else 
                     cur[j] = 0;
             }
-            prev = cur.clone();
+            prev = cur;
         }
         return prev[m];
     }
