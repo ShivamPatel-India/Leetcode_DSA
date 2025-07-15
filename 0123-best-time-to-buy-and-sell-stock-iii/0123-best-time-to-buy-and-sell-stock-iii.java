64a5c57ceb3e08168e7970281dba9193560f9d5c
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-15 02:33:17 -0700

    Time: 167 ms (7.27%), Space: 83.3 MB (6.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,60 +1,25 @@
 class Solution {
-    public int f(int[]p, int i, int buy, int cap, int n, int[][][] dp) {
-        if(cap == 0) return 0;
+    public int f(int[] p, int k, int n, int[][][] dp, int i, int buy) {
+        if(k==0) return 0;
         if(i == n) return 0;
-
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
 
         if(buy == 1) {
-            return dp[i][buy][cap] = Math.max(-p[i] + f(p, i+1, 0, cap, n, dp), 0 + f(p, i+1, 1, cap, n, dp));
+            return dp[i][buy][k] = Math.max(-p[i] + f(p, k, n, dp, i+1, 0), f(p, k, n, dp, i+1, 1));
         } else {
-            return dp[i][buy][cap] = Math.max(p[i] + f(p, i+1, 1, cap-1, n, dp), 0 + f(p, i+1, 0, cap, n, dp));
+            return dp[i][buy][k] = Math.max(p[i] + f(p, k-1, n, dp, i+1, 1), f(p, k, n, dp, i+1, 0));
         }
-    }
-
-    public int f_tabulation(int[] p) {
-        int n = p.length;
-        int[][][] dp = new int[n+1][2][3];
-
-        // base cases:
-        // cap = 0
-        for(int i = 0; i <= n; i++) {
-            for(int buy = 1; buy <= 1; buy++) {
-                dp[i][buy][0] = 0;
-            }
-        }
-        // index = n
-        for(int buy = 0; buy <= 1; buy++) {
-            for(int cap = 0; cap <=2 ; cap++) {
-                dp[n][buy][cap] = 0;
-            }
-        }
-
-        for(int i = n-1; i >= 0; i--) {
-            for(int buy = 0; buy <= 1; buy++) {
-                for(int cap = 1; cap <= 2; cap++) {
-                    if(buy == 1) {
-                        dp[i][buy][cap] = Math.max(-p[i] + dp[i+1][0][cap], 0 + dp[i+1][1][cap]);
-                    } else if(buy == 0) {
-                        dp[i][buy][cap] = Math.max(p[i] + dp[i+1][1][cap-1], 0 + dp[i+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][2];
     }
     public int maxProfit(int[] prices) {
-        // int cap = 2;
-        // int n = prices.length;
-        // int[][][] dp = new int[n][2][3];
-        
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < 2; j++) {
-        //         Arrays.fill(dp[i][j], -1);
-        //     }
-        // }
-        // return f(prices, 0, 1, cap, n, dp);
+        int k = 2;
+        int n = prices.length;
 
-        return f_tabulation(prices);
+        int[][][] dp = new int[n][2][k+1];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return f(prices, k, n, dp, 0 ,1);
     }
 }
