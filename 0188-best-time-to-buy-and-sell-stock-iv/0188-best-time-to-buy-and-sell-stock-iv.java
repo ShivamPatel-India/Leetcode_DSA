2f1cc589cd6ea51a90e56f9cdffa5494513a6694
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-15 01:55:56 -0700

    Time: 7 ms (43.97%), Space: 44.8 MB (44.14%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int f(int[]p, int i, int buy, int cap, int n, int[][][] dp) {
+        if(cap == 0) return 0;
+        if(i == n) return 0;
+
+        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+
+        if(buy == 1) {
+            return dp[i][buy][cap] = Math.max(-p[i] + f(p, i+1, 0, cap, n, dp), 0 + f(p, i+1, 1, cap, n, dp));
+        } else {
+            return dp[i][buy][cap] = Math.max(p[i] + f(p, i+1, 1, cap-1, n, dp), 0 + f(p, i+1, 0, cap, n, dp));
+        }
+    }
+
+    public int f_tabulation(int[] p, int k) {
+        int n = p.length;
+        int[][][] dp = new int[n+1][2][k+1];
+
+        // base cases:
+        // cap = 0
+        for(int i = 0; i <= n; i++) {
+            for(int buy = 1; buy <= 1; buy++) {
+                dp[i][buy][0] = 0;
+            }
+        }
+        // index = n
+        for(int buy = 0; buy <= 1; buy++) {
+            for(int cap = 0; cap <=k ; cap++) {
+                dp[n][buy][cap] = 0;
+            }
+        }
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cap = 1; cap <= k; cap++) {
+                    if(buy == 1) {
+                        dp[i][buy][cap] = Math.max(-p[i] + dp[i+1][0][cap], 0 + dp[i+1][1][cap]);
+                    } else if(buy == 0) {
+                        dp[i][buy][cap] = Math.max(p[i] + dp[i+1][1][cap-1], 0 + dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+    public int maxProfit(int k, int[] prices) {
+        // int cap = 2;
+        // int n = prices.length;
+        // int[][][] dp = new int[n][2][3];
+        
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < 2; j++) {
+        //         Arrays.fill(dp[i][j], -1);
+        //     }
+        // }
+        // return f(prices, 0, 1, cap, n, dp);
+
+        return f_tabulation(prices,k);
+    }
+}
