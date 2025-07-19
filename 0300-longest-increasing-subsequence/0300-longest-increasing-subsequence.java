861d9006e8011a3f743089bae9f110e6e9a9e71f
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-19 12:45:18 -0700

    Time: 71 ms (26.46%), Space: 77.4 MB (18.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -14,6 +14,7 @@ class Solution {
     }
     public int memoized(int[] nums, int n, int i, int pi, int[][] dp) {
         // recusion with memoization
+        // TC = O(n*n), SC = O(n*n) + O(n) // dp array + recursion stack space
         if(i == n) return 0;
         if(dp[i][pi+1] != -1) return dp[i][pi+1];
 
@@ -24,13 +25,32 @@ class Solution {
         }
         return dp[i][pi+1] = Math.max(take, notTake);
     }
+
+    public int tabulation(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int prev = i-1; prev >= -1; prev--) {
+                int notTake = 0 + dp[i+1][prev+1];
+                int take = 0;
+                if(prev == -1 || nums[i] > nums[prev]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+                dp[i][prev+1] = Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
+    }
     public int lengthOfLIS(int[] nums) {
         // return f(nums, 0, -1);
-        int n = nums.length;
+        
+        // int n = nums.length;
+        // // dp array for memoization with co-ordinate shift bcz arrays dont support -1 index
+        // int[][] dp = new int[n][n+1];
+        // for(int[] row: dp) Arrays.fill(row, -1);
+        // return memoized(nums, n, 0, -1, dp);
 
-        // dp array for memoization with co-ordinate shift bcz arrays dont support -1 index
-        int[][] dp = new int[n][n+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return memoized(nums, n, 0, -1, dp);
+        return tabulation(nums);
     }
 }
