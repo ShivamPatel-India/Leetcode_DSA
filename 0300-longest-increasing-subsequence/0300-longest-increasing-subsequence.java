8578bf3c5db58fd5d6ec2ab921ce3094378f02a1
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-19 21:36:29 -0700

    Time: 36 ms (75.87%), Space: 44.2 MB (65.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int lengthOfLIS(int[] nums) {
         /** This method contains function calls to other methods, each representing different way to solve LIS */
@@ -15,6 +16,9 @@ class Solution {
         // return spaceOptimized(nums);
 
         return mostOptimized(nums);
+
+        // printLIS(nums);
+        // return -1;
     }
     public int f(int[] nums, int i, int pi) {
         // pure recursive solution gives TLE
@@ -97,5 +101,38 @@ class Solution {
             maxi = Math.max(lis, maxi);
         }
         return maxi;
+    }
+    public void printLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int[] hash = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            hash[i] = i;
+            for(int prev = 0; prev < i; prev++) {
+                if(nums[i] > nums[prev] && 1 + dp[prev] > dp[i]) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
+        }
+        int lastIndex = -1;
+        int ans = -1;
+        for(int i = 0; i < n; i++) {
+            if(dp[i] > ans) {
+                ans = dp[i];
+                lastIndex = i;
+            }
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(nums[lastIndex]);
+
+        while(hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            temp.add(nums[lastIndex]);
+        }
+        Collections.sort(temp);
+        System.out.println(temp);
     }
 }
