c20096cdc198cc31a422af4079f3ab1ff876cdf5
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-25 00:55:48 -0700

    Time: 19 ms (31.93%), Space: 44.9 MB (98.21%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -11,12 +11,44 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+        if(root == null) return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            if(node == null) {
+                res.append("n ");
+                continue;
+            }
+            res.append(node.val + " ");
+            q.add(node.left);
+            q.add(node.right);
+        }
+        return res.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        if(data == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] nodes = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        q.add(root);
+        for(int i=1; i<nodes.length; i++) {
+            TreeNode parent = q.poll();
+            if(!nodes[i].equals("n")) {
+                TreeNode left = new TreeNode(Integer.parseInt(nodes[i]));
+                parent.left = left;
+                q.add(left);
+            }
+            if(!nodes[++i].equals("n")) {
+                TreeNode right = new TreeNode(Integer.parseInt(nodes[i]));
+                parent.right = right;
+                q.add(right);
+            }
+        }
+        return root;
     }
 }
 
