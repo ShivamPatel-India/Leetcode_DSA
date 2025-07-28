8b4b82f61d946f3815c7d0bf80bb3ff2f26eea6e
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-28 04:29:45 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,58 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfsOfGraph(adj);
-            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public static void dfs(int node, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer>
-    ans, boolean[] vis) {
+    // Function to return a list containing the DFS traversal of the graph.
+    public void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans) {
         vis[node] = true;
         ans.add(node);
         
-        for(Integer n: adj.get(node)) {
-            if(vis[n] == false) {
-                dfs(n, adj, ans, vis);
+        for(Integer it: adj.get(node)) {
+            if(vis[it] == false) {
+                dfs(it, adj, vis, ans);
             }
         }
     }
-    
-    // Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
-        int n = adj.size();
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        int V = adj.size();
+        boolean[] vis = new boolean[V];
         ArrayList<Integer> ans = new ArrayList<>();
-        boolean[] vis = new boolean[n];
-        dfs(0, adj, ans, vis);
+        dfs(0, adj, vis, ans);
         return ans;
     }
 }
