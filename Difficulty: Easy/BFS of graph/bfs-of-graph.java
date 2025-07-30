6b8b758710de4878a288a68905802a12b24d6529
ShivamPatel <shivampatel.sp515@gmail.com>
2025-07-29 23:10:37 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,26 +1,25 @@
 class Solution {
     // Function to return Breadth First Search Traversal of given graph.
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> bfs = new ArrayList<>();
-        int n = adj.size();
-        boolean[] vis = new boolean[n];
+        ArrayList<Integer> ans = new ArrayList<>();
+        int V = adj.size();
+        boolean[] vis = new boolean[V];
         Queue<Integer> q = new LinkedList<>();
         
         q.add(0);
         vis[0] = true;
         
         while(!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
+            int node = q.poll();
+            ans.add(node);
             
-            for(Integer it: adj.get(node)) {
-                if(vis[it] == false) {
-                     q.add(it);
-                     vis[it] = true;
+            for(int it: adj.get(node)) {
+                if(!vis[it]) {
+                    vis[it] = true;
+                    q.add(it);
                 }
             }
         }
-        return bfs;
-        
+        return ans;
     }
 }
