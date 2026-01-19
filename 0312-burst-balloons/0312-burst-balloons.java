cc3425760dae25a9130280448ccf7d60046c5418
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-19 05:24:36 -0800

    Time: 79 ms (54.75%), Space: 44.5 MB (55.86%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,30 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public int maxCoins(int i, int j, ArrayList<Integer> nums, int[][] dp) {
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+    public int maxCoins(ArrayList<Integer> nums) {
+        int n = nums.size();
+        nums.add(0,1);
+        nums.add(1);
 
-        int maxi = Integer.MIN_VALUE;
-        for(int ind = i; ind <= j; ind++) {
-            int coins = (nums.get(ind) * nums.get(i-1) * nums.get(j+1)) 
-                            + maxCoins(i, ind-1, nums, dp) 
-                            + maxCoins(ind+1, j, nums, dp);
-            maxi = Math.max(maxi, coins);
+        int[][] dp = new int[n+2][n+2];
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= n; j++) {
+                if(i > j) continue;
+                int maxi = Integer.MIN_VALUE;
+                for(int ind = i; ind <= j; ind++) {
+                    int cost = nums.get(i-1) * nums.get(ind) * nums.get(j+1) 
+                                + dp[i][ind-1] + dp[ind+1][j];
+                    maxi = Math.max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            } 
         }
-        return dp[i][j] = maxi;
+        return dp[1][n];
     }
     public int maxCoins(int[] arr) {
         ArrayList<Integer> nums = Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
-        nums.add(1);
-        nums.add(0,1);
-
-        int n = arr.length;
-        int[][] dp = new int[n+2][n+2];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        return maxCoins(1, n, nums, dp);
+        return maxCoins(nums);
     }
 
 }
