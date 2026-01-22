51985effefb00eb54ea2ba5f20bde8f86b86bee1
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-22 05:29:41 -0800

    Time: 0 ms (100%), Space: 43.9 MB (70.6%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,38 +1,23 @@
 class Solution {
-    // Function to search target in 2D matrix using binary search
     public boolean searchMatrix(int[][] matrix, int target) {
-        // Get the number of rows
         int n = matrix.length;
-
-        // Get the number of columns
         int m = matrix[0].length;
 
-        // Set initial binary search range
-        int low = 0, high = n * m - 1;
+        int low = 0;
+        int high = n*m - 1;
 
-        // Perform binary search
-        while (low <= high) {
-            // Calculate middle index
-            int mid = (low + high) / 2;
+        while(low <= high) {
+            int mid = (low+high)/2;
 
-            // Map 1D index to 2D coordinates
-            int row = mid / m;
-            int col = mid % m;
+            int row = mid/m;
+            int col = mid%m;
 
-            // Check if target is found
-            if (matrix[row][col] == target)
-                return true;
+            if(matrix[row][col] == target) return true;
 
-            // Discard left half
-            else if (matrix[row][col] < target)
-                low = mid + 1;
+            if(matrix[row][col] < target) low = mid+1;
 
-            // Discard right half
-            else
-                high = mid - 1;
+            else high = mid -1;
         }
-
-        // Target not found
         return false;
     }
 }
