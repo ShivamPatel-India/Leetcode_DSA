a8abf78f7e1071ec9a05e991898b339d000cbe34
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-22 17:10:51 -0800

    Time: 0 ms (100%), Space: 116.1 MB (73.27%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,52 +1,38 @@
 class Solution {
-    // Helper function to find the index of the row 
-    // with the maximum element in a given column
     public int maxElement(int[][] arr, int col) {
         int n = arr.length;
-        int max = Integer.MIN_VALUE;
+        int maxi = Integer.MIN_VALUE;
         int index = -1;
 
-        // Iterate through each row to find the maximum element 
-        // in the specified column
-        for (int i = 0; i < n; i++) {
-            if (arr[i][col] > max) {
-                max = arr[i][col];
+        for(int i = 0; i < n; i++) {
+            if(arr[i][col] > maxi) {
+                maxi = arr[i][col];
                 index = i;
             }
         }
         return index;
     }
-
-    public int[] findPeakGrid(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
+    public int[] findPeakGrid(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
 
         int low = 0;
-        int high = m - 1;
+        int high = m-1;
 
-        // Perform binary search on columns
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while(low <= high) {
+            int mid = (high + low) / 2;
 
-            // Find the index of the row with the maximum element 
-            // in the middle column
-            int row = maxElement(arr, mid);
+            int row = maxElement(mat, mid);
 
-            // Determine left and right neighbors of middle element
-            int left = mid - 1 >= 0 ? arr[row][mid - 1] : Integer.MIN_VALUE;
-            int right = mid + 1 < m ? arr[row][mid + 1] : Integer.MIN_VALUE;
+            int left = mid-1 >= 0 ? mat[row][mid-1] : Integer.MIN_VALUE;
+            int right = mid+1 < m ? mat[row][mid+1] : Integer.MIN_VALUE;
 
-            // Check if the middle element is a peak
-            if (arr[row][mid] > left && arr[row][mid] > right) {
+            if(mat[row][mid] > left && mat[row][mid] > right) {
                 return new int[]{row, mid};
-            } else if (left > arr[row][mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
             }
+            else if(mat[row][mid] < left) high = mid-1;
+            else low = mid+1;
         }
-
-        // Return [-1, -1] if no peak element is found
-        return new int[]{-1, -1};
+        return new int[]{-1,-1};
     }
 }
