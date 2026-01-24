ce5b04642313d0afcd0277ffd6e7bc7279a3e74c
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-24 03:04:52 -0800

    Time: 4 ms (18.68%), Space: 46.7 MB (64.59%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        int n = nums.length;
+        int[] res = Arrays.copyOf(nums, n);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> {
+            if(res[a] != res[b]) return Integer.compare(res[a], res[b]);
+            return Integer.compare(a,b);
+        });
+
+        for(int i = 0; i < n; i++) {
+            minHeap.add(i);
+        }
+
+        for(int i = 1; i <=k; i++) {
+            int ind = minHeap.poll();
+            res[ind] *= multiplier;
+            minHeap.offer(ind);
+        }
+
+        return res;
+
+    }
+}
