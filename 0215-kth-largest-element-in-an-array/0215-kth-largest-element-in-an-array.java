65b0cc162bd1c673ff72ccf2be84fc0299da2457
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-24 20:59:00 -0800

    Time: 4 ms (97.16%), Space: 70.5 MB (8.2%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,14 +1,55 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (int num : nums) {
-            heap.add(num);
+    private int partition(int[] nums, int left, int right) {
+        int mid = (left + right) >> 1;
+        swap(nums, mid, left + 1);
 
-            if (heap.size() > k) {
-                heap.poll();
+        if (nums[left] < nums[right])
+            swap(nums, left, right);
+        if (nums[left + 1] < nums[right])
+            swap(nums, left + 1, right);
+        if (nums[left] < nums[left + 1])
+            swap(nums, left, left + 1);
 
-            }
+        int pivot = nums[left + 1];
+        int i = left + 1;
+        int j = right;
+
+        while (true) {
+            while (nums[++i] > pivot);
+            while (nums[--j] < pivot);
+            if (i > j) break;
+            swap(nums, i, j);
         }
-        return heap.peek();
+
+        nums[left + 1] = nums[j];
+        nums[j] = pivot;
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private int quickSelect(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (true) {
+            if (right <= left + 1) {
+                if (right == left + 1 && nums[right] > nums[left])
+                    swap(nums, left, right);
+                return nums[k];
+            }
+
+            int j = partition(nums, left, right);
+
+            if (j >= k) right = j - 1;
+            if (j <= k) left = j + 1;
+        }
+    }
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, k - 1);
     }
 }
