556149f21fd9280de3448d4c80c6b6f67f926bd3
ShivamPatel <shivampatel.sp515@gmail.com>
2026-01-28 15:10:36 -0800

    Time: 32 ms (63.02%), Space: 57 MB (19.6%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,113 +1,91 @@
-import java.util.*;
-class Tweet implements Comparable<Tweet>{
+class Tweet implements Comparable<Tweet> {
     int time;
     int tweetId;
-    Tweet(int t, int id){
-        time = t;
-        tweetId = id;
+    Tweet(int time, int tweetId) {
+        this.time = time;
+        this.tweetId = tweetId;
     }
-    public int compareTo(Tweet that){
-        return that.time - this.time; //dec
+
+    public int compareTo(Tweet that) {
+        return that.time - this.time;
     }
 }
+
 class User {
     int userId;
-    HashSet<Integer> followers;
+    HashSet<Integer> followees;
     List<Tweet> tweets;
-    User(int userId){
+    User(int userId) {
         this.userId = userId;
-        followers = new HashSet<>();
-        followers.add(userId);//add self also as follower;
+        followees = new HashSet<>();
+        followees.add(userId);
         tweets = new LinkedList<>();
+    } 
+    public void addTweet(Tweet tweet) {
+        tweets.add(0, tweet);
     }
-    public void addTweet(Tweet t){
-        tweets.add(0,t); //insertion at the head
+    public void addFollowee(int followeeId) {
+        followees.add(followeeId);
     }
-    public void addFollower(int followeeId){
-        followers.add(followeeId);
+    public void removeFollowee(int followeeId) {
+        followees.remove(followeeId);
     }
-    public void removeFollower(int followeeId){
-        followers.remove(followeeId);
-    }
+
+
 }
 class Twitter {
-    HashMap<Integer,User> userMap;
+    HashMap<Integer, User> userMap;
     int timeCounter;
     public Twitter() {
-        userMap = new HashMap<>();
-        timeCounter=0;
+        userMap = new HashMap<>(0);
+        timeCounter = 0;
     }
     
     public void postTweet(int userId, int tweetId) {
         timeCounter++;
-        //const
-        if(!userMap.containsKey(userId)){
-            userMap.put(userId,new User(userId));
+        if(!userMap.containsKey(userId)) {
+            userMap.put(userId, new User(userId));
         }
-        //const
+
         User user = userMap.get(userId);
-        user.addTweet(new Tweet(timeCounter,tweetId));
+        user.addTweet(new Tweet(timeCounter, tweetId));
     }
-    // N*(10log10) 
+    
     public List<Integer> getNewsFeed(int userId) {
-        //const
-        if(!userMap.containsKey(userId)){
-            return new ArrayList<>();
-        }
+        if(!userMap.containsKey(userId)) return new ArrayList<>();
         PriorityQueue<Tweet> pq = new PriorityQueue<>();
-        //add self tweets + followers tweets
+
         User user = userMap.get(userId);
-        // N*10log10
-        for(int followerId : user.followers){ //N
-            int count=0;
-            for(Tweet tweet : userMap.get(followerId).tweets){ //T
-                pq.offer(tweet); //logT
+        for(int followee: user.followees) {
+            int count = 0;
+            for(Tweet tweet: userMap.get(followee).tweets) {
+                pq.offer(tweet);
                 count++;
-                if(count>10){
-                    break;
-                }
+                if(count > 10) break;
             }
-        } 
-        // 10log10
-        // int count=0;
-        // for(Tweet tweet : user.tweets){
-        //     pq.offer(tweet);
-        //     count++;
-        //     if(count>10){
-        //         break;
-        //     }
-        // }
-        //10log10
+        }
         List<Integer> res = new ArrayList<>();
-        int index=0;
-        while(!pq.isEmpty() && index<10){
-            Tweet tweet = pq.poll();
-            res.add(tweet.tweetId);
+        int index = 0;
+        while(!pq.isEmpty() && index < 10) {
+            res.add(pq.poll().tweetId);
             index++;
         }
         return res;
-
     }
     
     public void follow(int followerId, int followeeId) {
-        //const
-        if(!userMap.containsKey(followerId)){
-            userMap.put(followerId,new User(followerId));
-        }
-        if(!userMap.containsKey(followeeId)){
-            userMap.put(followeeId,new User(followeeId));
-        }
+        if(!userMap.containsKey(followerId)) userMap.put(followerId, new User(followerId));
+        if(!userMap.containsKey(followeeId)) userMap.put(followeeId, new User(followeeId));
+
         User user = userMap.get(followerId);
-        user.addFollower(followeeId);
+        user.addFollowee(followeeId);
     }
     
     public void unfollow(int followerId, int followeeId) {
-        //const
-        if(!userMap.containsKey(followerId) || !userMap.containsKey(followeeId)){
-            return;
-        }
+        if(!userMap.containsKey(followerId) || !userMap.containsKey(followeeId)) return;
+
         User user = userMap.get(followerId);
-        user.removeFollower(followeeId);
+        user.removeFollowee(followeeId);
     }
 }
 
