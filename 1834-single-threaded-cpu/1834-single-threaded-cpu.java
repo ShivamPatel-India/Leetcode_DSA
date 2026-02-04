069071eb4540c9bff343131d05591cbe5ec068da
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-04 10:06:56 -0800

    Time: 163 ms (17.97%), Space: 144.8 MB (75.67%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,37 +1,38 @@
-public class Solution {
+import java.util.*;
+
+class Solution {
     public int[] getOrder(int[][] tasks) {
         int n = tasks.length;
-        Integer[] indices = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            indices[i] = i;
+        Integer[] indicies = new Integer[n];
+        for(int i = 0; i < n; i++) {
+            indicies[i] = i;
         }
 
-        Arrays.sort(indices, (a, b) ->
-            tasks[a][0] != tasks[b][0] ? tasks[a][0] - tasks[b][0] : a - b
+        Arrays.sort(indicies, (a,b) -> 
+            tasks[a][0] != tasks[b][0] ? tasks[a][0] - tasks[b][0] : a-b
         );
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) ->
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> 
             tasks[a][1] != tasks[b][1] ? tasks[a][1] - tasks[b][1] : a - b
         );
 
         int[] result = new int[n];
         long time = 0;
         int i = 0, resIndex = 0;
-        while (!minHeap.isEmpty() || i < n) {
-            while (i < n && tasks[indices[i]][0] <= time) {
-                minHeap.offer(indices[i]);
+        while(!minHeap.isEmpty() || i < n) {
+            while(i < n && tasks[indicies[i]][0] <= time) {
+                minHeap.offer(indicies[i]);
                 i++;
             }
 
-            if (minHeap.isEmpty()) {
-                time = tasks[indices[i]][0];
+            if(minHeap.isEmpty()) {
+                time = tasks[indicies[i]][0];
             } else {
                 int nextIndex = minHeap.poll();
                 time += tasks[nextIndex][1];
                 result[resIndex++] = nextIndex;
             }
         }
-
         return result;
     }
 }
