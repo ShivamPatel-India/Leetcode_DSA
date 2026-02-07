ad3eb4d2921b97eaa17564e1e553a09912a3f0f4
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-07 13:17:16 -0800

    Time: 1 ms (99.15%), Space: 65.5 MB (63.16%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,20 +1,10 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
         int spaces = k;
-        for(int num: nums) {
-            if(num == 1) {
-                if(spaces < k) return false;
-                spaces = 0;
-            } else spaces += 1;
-        }
+        for(int n: nums) {
+            if(n == 1) if(spaces < k) return false; else spaces = 0;
+            else spaces += 1;
+        } 
         return true;
-    }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
     }
 }
