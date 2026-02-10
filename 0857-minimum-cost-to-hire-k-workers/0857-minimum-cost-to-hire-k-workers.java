255147bb2333afe085bd57e5718e46b10f9ef576
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-09 20:40:40 -0800

    Time: 37 ms (30.87%), Space: 48.1 MB (70.92%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
@@ -1,26 +1,28 @@
 class Solution {
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
         int n = quality.length;
-        double res = Double.MAX_VALUE;
-        double totalQuality = 0;
-
         double[][] workers = new double[n][2];
+        double totalQuality = 0;
+        double res = Double.MAX_VALUE;
+
         for(int i = 0; i < n; i++) {
-            // [rate, quality]
             workers[i] = new double[]{(double) wage[i] / quality[i], (double) quality[i]};
         }
+        // sorting by the rate inorder to fix the smallest eatte possible
         Arrays.sort(workers, Comparator.comparingDouble(a -> a[0]));
+
+        // maxHeap to keep track of k smallest amouunt of qualities
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
         for(double[] worker: workers) {
-            double ratio = worker[0];
+            double rate = worker[0];
             int q = (int) worker[1];
 
-            maxHeap.offer(q);
             totalQuality += q;
+            maxHeap.offer(q);
 
             if(maxHeap.size() > k) totalQuality -= maxHeap.poll();
-            if(maxHeap.size() == k) res = Math.min(res, totalQuality * ratio);
+            if(maxHeap.size() == k) res = Math.min(res, rate * totalQuality);
         }
         return res;
     }
