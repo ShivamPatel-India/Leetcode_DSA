c3e7d126f8a60a8fc3c910a00a4623e14a260c74
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-11 23:28:53 -0800

    Time: 11 ms (95.19%), Space: 51.7 MB (5.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,27 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // num itself-> key, freq of the number -> value
         Map<Integer, Integer> count = new HashMap<>();
-        List<Integer>[] freq = new List[nums.length+1];
+        List<Integer>[] freq = new List[nums.length + 1];
         
-        for(int i = 0; i < freq.length; i++) {
-            freq[i] = new ArrayList<>();
-        }
-
+        // for any number frequency can not be greater than the size of the array.
+        // counting the freq.
         for(int num: nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
         }
-        for(Map.Entry<Integer,Integer> entry: count.entrySet()) {
+
+        for(int i = 0; i <= nums.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        // after the freq is counted, we will put the number to its corresponding freq
+        for(Map.Entry<Integer, Integer> entry: count.entrySet()) {
             freq[entry.getValue()].add(entry.getKey());
         }
 
         int[] res = new int[k];
         int index = 0;
-        for(int i = freq.length - 1; i >= 0 && index < k; i--) {
-            for(int n: freq[i]) {
-                res[index++] = n;
-                if(index == k) {
-                    return res;
-                }
+        for(int i = freq.length-1; i > 0; i--) {
+            for(int num: freq[i]) {
+                res[index++] = num;
+                if(index == k) return res;
             }
         }
         return res;
