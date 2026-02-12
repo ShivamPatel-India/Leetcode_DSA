8dcb58f5ea59813499820c7c51fe937dc1b0a9bb
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-11 23:51:15 -0800

    Time: 2 ms (99.46%), Space: 44.8 MB (57.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if given strings are of different length
-        if (s.length() != t.length())
-            return false;
-
-        // creating a frequency array as we 26 letters in alphabet asd as given in
-        // contraints that strings can have only lowercases
+        if(s.length() != t.length()) return false;
         int[] freq = new int[26];
-
-        // counting the freq of every letter in string s
-        for (char c : s.toCharArray()) freq[c-'a']++;
-        // decrease the freq of every letter in array for the chars in t
-        for (char c : t.toCharArray()) freq[c-'a']--;
-
-        // now if for every character if the frequency is 0 thann we can say that number
-        // of appearance of every character in the s and t are same
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0)
-                return false;
-        }
-        return true;
+        for(char c: s.toCharArray()) freq[c-'a']++;
+        for(char c: t.toCharArray()) freq[c-'a']--;
+        for(int i = 0; i < 26; i++) if (freq[i] != 0) return false;
+        return true; 
     }
 }
