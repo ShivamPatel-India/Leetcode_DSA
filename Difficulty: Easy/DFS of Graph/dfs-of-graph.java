68d58d995d49104f39fd4993b80b25daf72896f4
ShivamPatel <shivampatel.sp515@gmail.com>
2026-02-12 21:53:19 -0800

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,20 +1,20 @@
 class Solution {
-    // Function to return a list containing the DFS traversal of the graph.
-    public void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans) {
+    public void f(int node, boolean[] vis, ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ans) {
         vis[node] = true;
         ans.add(node);
         
-        for(Integer it: adj.get(node)) {
-            if(vis[it] == false) {
-                dfs(it, adj, vis, ans);
+        for(int it: adj.get(node)) {
+            if(!vis[it]) {
+                f(it, vis, adj, ans);
             }
         }
     }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
         int V = adj.size();
         boolean[] vis = new boolean[V];
-        ArrayList<Integer> ans = new ArrayList<>();
-        dfs(0, adj, vis, ans);
+        f(0, vis, adj, ans);
         return ans;
     }
 }
