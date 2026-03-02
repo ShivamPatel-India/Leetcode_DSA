661090f7f03fee205fbf52494e40417086a6acdd
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-02 07:38:07 -0800

    Time: 2 ms (91.12%), Space: 44 MB (92.43%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        int total_oranges = 0;
+
+        // count the total oranges and put the rotten oranges inside the Queue for BFS
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                // if the cell is not empty (rotten or fresh orange)
+                if(grid[i][j] != 0) total_oranges++; 
+                // if orange is rotten add it to the queue
+                if(grid[i][j] == 2) q.add(new int[]{i,j});
+            }
+        }
+
+        // some-preprocessing for checking the neighboring orange condition
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        int minTime = 0;
+        int explored_oranges = 0;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            explored_oranges += size;
+
+            for(int i = 0; i < size; i++) {
+                int[] point = q.poll();
+                for(int j = 0; j < 4; j++) {
+                    int x = point[0] + dx[j];
+                    int y = point[1] + dy[j];
+
+                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)
+                        continue;
+                    grid[x][y] = 2;
+                    q.offer(new int[]{x,y});
+                }
+            }
+            if(q.size() != 0) minTime++;
+        }
+        return total_oranges == explored_oranges ? minTime : -1;
+    }
+}
