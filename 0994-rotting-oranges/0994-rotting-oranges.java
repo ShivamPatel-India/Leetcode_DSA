f20e20e16db9113b4247fc3cf8ffed22ca12e49a
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-03 07:09:42 -0800

    Time: 2 ms (91.08%), Space: 44.2 MB (56.65%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,20 +3,20 @@ class Solution {
         int rows = grid.length;
         int cols = grid[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
         int total_oranges = 0;
+        Queue<int[]> q = new LinkedList<>();
 
-        // count the total oranges and put the rotten oranges inside the Queue for BFS
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                // if the cell is not empty (rotten or fresh orange)
-                if(grid[i][j] != 0) total_oranges++; 
-                // if orange is rotten add it to the queue
-                if(grid[i][j] == 2) q.add(new int[]{i,j});
+                if(grid[i][j] != 0) {
+                    total_oranges++;
+                }
+                if(grid[i][j] == 2) {
+                    q.offer(new int[]{i,j});
+                }
             }
         }
 
-        // some-preprocessing for checking the neighboring orange condition
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,-1,1};
         int minTime = 0;
@@ -26,20 +26,21 @@ class Solution {
             int size = q.size();
             explored_oranges += size;
 
-            for(int i = 0; i < size; i++) {
+            for(int i = 0 ; i < size; i++) {
                 int[] point = q.poll();
                 for(int j = 0; j < 4; j++) {
-                    int x = point[0] + dx[j];
-                    int y = point[1] + dy[j];
+                    int x = dx[j] + point[0];
+                    int y = dy[j] + point[1];
 
-                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y]==0 || grid[x][y]==2)
-                        continue;
+                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y] == 0 || 
+                            grid[x][y] == 2) continue;
+
                     grid[x][y] = 2;
                     q.offer(new int[]{x,y});
                 }
             }
             if(q.size() != 0) minTime++;
         }
-        return total_oranges == explored_oranges ? minTime : -1;
+        return explored_oranges == total_oranges ? minTime : -1;
     }
 }
