8637f5d5ee745804a9e8e5bd0f7df2b3b3457826
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-04 03:01:57 -0800

    Time: 15 ms (45.87%), Space: 49.9 MB (7.95%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,56 +1,53 @@
 class Solution {
     public class Node {
-        int first;
-        int second;
-        int third; 
-        Node(int _first, int _second, int _third) {
-            this.first = _first; 
-            this.second = _second; 
-            this.third = _third; 
+        int row, col, dist;
+        Node(int _row, int _col, int _dist) {
+            this.row = _row;
+            this.col = _col;
+            this.dist = _dist;
         }
     }
-    public int[][] updateMatrix(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
 
         int[][] vis = new int[n][m];
         int[][] dist = new int[n][m];
 
         Queue<Node> q = new LinkedList<>();
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 0) {
-                    q.add(new Node(i,j,0));
-                    vis[i][j] = 0;
+                if(mat[i][j] == 0) {
+                    q.add(new Node(i,j, 0));
+                    vis[i][j] = 0; // marking already visited nodes as 0
                 } else {
                     vis[i][j] = 1;
                 }
             }
         }
-        
-        int delRow[] = {-1,0,+1,0};
-        int delCol[] = {0,-1,0,+1};
+
+        int[] delRow = {-1,1,0,0};
+        int[] delCol = {0,0,-1,1};
 
         while(!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().second;
-            int steps = q.peek().third;
+            int row = q.peek().row;
+            int col = q.peek().col;
+            int steps = q.peek().dist;
             q.remove();
 
             dist[row][col] = steps;
 
-            for(int i = 0;i<4;i++) {
-	            int nrow = row + delRow[i]; 
-	            int ncol = col + delCol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-	            && vis[nrow][ncol] == 1)  {
-	                    vis[nrow][ncol] = 0; 
-    	                q.add(new Node(nrow, ncol, steps+1));  
-	                } 
-	            }
-	    }
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==1) {
+                    vis[nrow][ncol] = 0;
+                    q.add(new Node(nrow, ncol, steps + 1));
+                }
+            }
+        }
         return dist;
     }
 }
