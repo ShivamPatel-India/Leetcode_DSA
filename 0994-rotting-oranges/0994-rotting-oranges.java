53616ff65b3ccbced8ded47a9b033adc3619f28c
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-03 23:29:26 -0800

    Time: 2 ms (91.03%), Space: 44.3 MB (56.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -8,32 +8,30 @@ class Solution {
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(grid[i][j] != 0) {
-                    total_oranges++;
-                }
-                if(grid[i][j] == 2) {
-                    q.offer(new int[]{i,j});
-                }
+                if(grid[i][j] != 0) total_oranges++;
+                if(grid[i][j] == 2) q.offer(new int[]{i,j});
             }
         }
+
+        int explored_oranges = 0;
 
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,-1,1};
         int minTime = 0;
-        int explored_oranges = 0;
 
         while(!q.isEmpty()) {
             int size = q.size();
             explored_oranges += size;
 
-            for(int i = 0 ; i < size; i++) {
+            // rott all the oranges neighboring to the already rotten oranges
+            for(int i = 0; i < size; i++) {
                 int[] point = q.poll();
                 for(int j = 0; j < 4; j++) {
                     int x = dx[j] + point[0];
                     int y = dy[j] + point[1];
 
-                    if(x<0 || y<0 || x>=rows || y>=cols || grid[x][y] == 0 || 
-                            grid[x][y] == 2) continue;
+                    if(x<0 || y < 0 || x >= rows || y >= cols ||
+                    grid[x][y] == 0 || grid[x][y] == 2) continue;
 
                     grid[x][y] = 2;
                     q.offer(new int[]{x,y});
