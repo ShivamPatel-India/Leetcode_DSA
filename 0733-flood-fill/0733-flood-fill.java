b1694319d34f3faa507f71e3b56afaba3edffd68
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-04 00:46:44 -0800

    Time: 0 ms (100%), Space: 46.6 MB (96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,21 @@
 class Solution {
-    public void dfs(int[][] image, int[][] ans, int initialColor, int sr, int sc, int color, int[] dx, int[] dy) {  
-        ans[sr][sc] = color;
-        int rows = image.length;
-        int cols = image[0].length;
-
+    public void dfs(int row, int col, int[][] image, int[][] ans, int[] dx, int[] dy, int initialColor, int color) {
+        ans[row][col] = color;
         for(int i = 0; i < 4; i++) {
-            int x = dx[i] + sr;
-            int y = dy[i] + sc;
+            int x = dx[i] + row;
+            int y = dy[i] + col;
 
-            if(x>=0 && y>=0 && x<rows && y<cols && image[x][y]==initialColor && ans[x][y]!=color) dfs(image, ans, initialColor, x, y, color, dx, dy);
-        } 
+            if(x>=0 && y >=0 && x<image.length && y<image[0].length && image[x][y] == initialColor && ans[x][y] != color) {
+                dfs(x, y, image, ans, dx, dy, initialColor, color);
+            }
+        }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int initialColor = image[sr][sc];
         int ans[][] = image;
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        dfs(image, ans, initialColor, sr, sc, color, dx, dy);
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        dfs(sr, sc, image, ans, dx, dy, initialColor, color);
         return ans;
     }
 }
