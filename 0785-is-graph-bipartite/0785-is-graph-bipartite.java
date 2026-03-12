d821f022227b3f528080487b075a5e54bb7a351d
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-12 03:09:19 -0700

    Time: 2 ms (40.09%), Space: 46.8 MB (98.25%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,32 +1,36 @@
 class Solution {
-    public boolean dfs(int node, int[][] graph, int[] color, int colorCode) {
-        color[node] = colorCode;
-
-        for(int i = 0; i < graph[node].length; i++) {
-            int adjNode = graph[node][i];
-            if(color[adjNode] == -1) {
-                if(dfs(adjNode, graph, color, 1-colorCode) == false) return false;
-            }
-            else if(color[adjNode] == colorCode) return false;
-        }
-        return true;
-    }
     public boolean isBipartite(int[][] graph) {
         int V = graph.length;
         int[] color = new int[V];
 
-        // initially all the nodes are uncolored i.e., -1
-        for(int i = 0; i < V; i++) color[i] = -1;
-
-        // 1. colors being used: 0 and 1
-        // 2. graph may not be  connected so we need to consider all the components
         for(int i = 0; i < V; i++) {
-            if(color[i] == -1) {
-                if(dfs(i, graph, color, 0) == false) return false;
+            color[i] = -1;
+        }
+        
+        // queue will contain adjacent node and its color
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0; i < V; i++) {
+            if(color[i] != -1) continue; // already visited so skip
+
+            // starting fresh BFS with every new component
+            q.offer(new int[]{i,0});
+            color[i] = 0;
+
+            while(!q.isEmpty()) {
+                int node = q.peek()[0];
+                int nodeColor = q.peek()[1];
+                q.poll();
+
+                for(int adjNode: graph[node]) {
+                    if(color[adjNode] == -1) {
+                        q.offer(new int[]{adjNode, 1-nodeColor});
+                        color[adjNode] = 1-nodeColor;
+                    } else if(color[adjNode] == nodeColor) {
+                        return false;
+                    }
+                }
             }
         }
         return true;
-
-
     }
 }
