469146bfa4d441c97fd8d33fc05c5412e5141828
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-16 05:13:02 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,69 +1,39 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to detect cycle in an undirected graph.
-    public static boolean checkForCycle(ArrayList<ArrayList<Integer>> adj,
-    boolean[] vis, int node, int V) {
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{node, -1});
+    private boolean detectCycle(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
         vis[node] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{node, -1});
         
         while(!q.isEmpty()) {
-            int n = q.peek()[0];
-            int parent = q.peek()[1];
-            q.remove();
+            int currentNode = q.peek()[0];
+            int parentNode = q.peek()[1];
+            q.poll();
             
-            for(Integer i: adj.get(n)) {
-                if(!vis[i]) {
-                    vis[i] = true;
-                    q.add(new int[]{i,n});
-                } else if(i != parent) return true;
+            for(int adjNode: adj.get(currentNode)) {
+                if(!vis[adjNode]) {
+                    vis[adjNode] = true;
+                    q.offer(new int[]{adjNode, currentNode});
+                } else if (parentNode != adjNode) {
+                    return true;
+                }
             }
         }
         return false;
     }
-    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
-        int V = adj.size();
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for(int[] edge: edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
         boolean[] vis = new boolean[V];
         
         for(int i = 0; i < V; i++) {
             if(!vis[i]) {
-                if(checkForCycle(adj, vis, i, V)) return true;
+                if(detectCycle(i, adj, vis)) return true;
             }
         }
         return false;
