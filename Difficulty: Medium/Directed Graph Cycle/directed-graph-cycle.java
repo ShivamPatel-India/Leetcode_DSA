06187cc59e6e9d891af616e05cc0b6b49149e7ea
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-17 03:32:33 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,34 +1,31 @@
 class Solution {
-    private boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] vis, int[] curPath) {
-        vis[node] = 1;
-        curPath[node] = 1;
+    public boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] pathVis) {
+        vis[node] = true;
+        pathVis[node] = true;
         
         for(int adjNode: adj.get(node)) {
-            if(vis[adjNode]==0) {
-                if(dfs(adjNode, adj, vis, curPath)) return true;
-            } else if(curPath[adjNode] == 1) {
-                return true;
-            }
+            if(!vis[adjNode]) {
+                if(dfs(adjNode, adj, vis, pathVis)) return true;
+            } else if(pathVis[adjNode])return true;
         }
-        curPath[node] = 0;
+        pathVis[node] = false;
         return false;
     }
     public boolean isCyclic(int V, int[][] edges) {
-        // create a adjList out of edges
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
         
-        // directed graph
-        for(int[] edge: edges) {
-            adj.get(edge[0]).add(edge[1]);
-        }
+        for(int[] edge: edges) adj.get(edge[0]).add(edge[1]);
         
-        int[] vis = new int[V];
-        int[] curPath = new int[V];
+        // to keep track of the already visited nodes
+        boolean[] vis = new boolean[V];
+        
+        // to keep track of the already visited nodes in the current path
+        boolean[] pathVis = new boolean[V];
         
         for(int i = 0; i < V; i++) {
-            if(vis[i]==0) {
-                if(dfs(i, adj, vis, curPath)) return true;
+            if(!vis[i]) {
+                if(dfs(i, adj, vis, pathVis)) return true;
             }
         }
         return false;
