42b4e9bea793c67f8a811d870317230c48f5b215
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-19 03:00:23 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
@@ -9,54 +9,48 @@ class Solution {
     }
     private void dfs(int node, List<List<Pair>> adj, boolean[] vis, Stack<Integer> st) {
         vis[node] = true;
-        
-        for(Pair pair: adj.get(node)) {
-            int adjNode = pair.v;
+        for(Pair p: adj.get(node)) {
+            int adjNode = p.v;
+            
             if(!vis[adjNode]) dfs(adjNode, adj, vis, st);
         }
         st.add(node);
     }
     public int[] shortestPath(int V, int E, int[][] edges) {
-        // need to create an adjList out of edges array
         List<List<Pair>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        
-        for(int edge[]: edges) {
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
-            adj.get(u).add(new Pair(v, wt));
-        }
-        
-        // now perform a topo sort and store it in stack
-        Stack<Integer> st = new Stack<>();
-        boolean[] vis = new boolean[V];
         for(int i = 0; i < V; i++) {
-            if(!vis[i]) dfs(i, adj, vis, st);
+            adj.add(new ArrayList<>());
         }
         
         int[] dist = new int[V];
         Arrays.fill(dist, (int)1e9);
-        
         dist[0] = 0;
+        
+        for(int[] edge: edges) {
+            adj.get(edge[0]).add(new Pair(edge[1],edge[2]));
+        }
+        
+        boolean[] vis = new boolean[V];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < V; i++) {
+            if(!vis[i]) dfs(i, adj, vis, st);
+        }
+        
         while(!st.isEmpty()) {
             int node = st.pop();
-            int d = dist[node];
             
             for(Pair p: adj.get(node)) {
                 int adjNode = p.v;
                 int wt = p.wt;
                 
-                if(d + wt < dist[adjNode]) {
-                    dist[adjNode] = d + wt;
+                if(dist[adjNode] > dist[node] + wt) {
+                    dist[adjNode] = dist[node] + wt;
                 }
             }
         }
-        
         for(int i = 0; i < V; i++) {
             if(dist[i] == (int)1e9) dist[i] = -1;
         }
-        
         return dist;
     }
 }
