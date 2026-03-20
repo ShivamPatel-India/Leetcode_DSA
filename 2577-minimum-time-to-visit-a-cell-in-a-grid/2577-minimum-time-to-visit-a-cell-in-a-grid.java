2d05cdcde851f04aa8942414f35888acb8f70e0f
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-20 14:49:54 -0700

    Time: 193 ms (33.17%), Space: 85.5 MB (72.11%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.java
@@ -1,51 +1,45 @@
 class Solution {
     public int minimumTime(int[][] grid) {
         if(grid[0][1] > 1 && grid[1][0] > 1) return -1;
-
-        int m = grid.length; // row
-        int n = grid[0].length; // column
+        
+        int m = grid.length;
+        int n = grid[0].length;
 
         int[][] time = new int[m][n];
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                time[i][j] = (int)1e9;
-            }
+            Arrays.fill(time[i], (int)1e9);
         }
 
         int[] delRow = {1, -1, 0, 0};
         int[] delCol = {0, 0, 1, -1};
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x,y) -> x[2] - y[2]);
-        pq.add(new int[]{0,0,0}); // [x, y, t]
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[2] - y[2]);
+        pq.add(new int[]{0,0,0}); // [x, y, time]
         time[0][0] = 0;
 
         while(!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int x = curr[0];
-            int y = curr[1];
-            int t = curr[2];
-
-            if(x == m-1 && y == n-1) return t;
+            int[] cur = pq.poll();
+            int x = cur[0], y = cur[1], t = cur[2];
 
             for(int i = 0; i < 4; i++) {
                 int nx = x + delRow[i];
                 int ny = y + delCol[i];
+
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
                 
                 int newTime = t + 1;
+                // if neighboring node is not visitable then we have to wait 
+                if(newTime < grid[nx][ny]) {
+                    int diff = grid[nx][ny] - newTime;
+                    newTime = grid[nx][ny] + (diff % 2 == 0 ? 0 : 1);
+                }
 
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n) {
-                    if(newTime < grid[nx][ny]) {
-                        // in this scenario we need t implement a bounce mechanism
-                        int diff = grid[nx][ny] - newTime;
-                        newTime =  grid[nx][ny] + (diff % 2 == 0 ? 0 : 1);
-                    }
-                    if(newTime >= grid[nx][ny] && newTime < time[nx][ny]) {
-                        time[nx][ny] = newTime;
-                        pq.add(new int[]{nx, ny, newTime});
-                    }
+                if(newTime < time[nx][ny]) {
+                    time[nx][ny] = newTime;
+                    pq.add(new int[]{nx, ny, newTime});
                 }
             }
         }
         return time[m-1][n-1] == (int)1e9 ? -1 : time[m-1][n-1];
-
     }
 }
