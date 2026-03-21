6647349004563066c331d9102d87fc1ab0c64969
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-21 10:27:58 -0700

    Time: 10 ms (86.46%), Space: 49.7 MB (29.84%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,46 +1,43 @@
 class Solution {
     class Pair {
         int node;
-        int time; 
-        Pair(int _node, int _time) {
-            this.node = _node;
-            this.time = _time;
+        int time;
+        Pair(int node, int time) {
+            this.node = node;
+            this.time = time;
         }
     }
     public int networkDelayTime(int[][] times, int n, int k) {
         List<List<Pair>> adj = new ArrayList<>();
-        for(int  i = 0 ; i <= n; i ++) adj.add(new ArrayList<>());
-
-        for(int[] time: times) {
-            adj.get(time[0]).add(new Pair(time[1],time[2]));
+        for(int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        int[] time = new int[n+1];
-        Arrays.fill(time, (int)1e9);
+        for(int[] time: times) adj.get(time[0]).add(new Pair(time[1], time[2]));
+        int[] t = new int[n+1];
+        Arrays.fill(t, (int)1e9);
 
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(k, 0));
-        time[k] = 0;
+        t[k] = 0;
 
         while(!q.isEmpty()) {
-            int node = q.peek().node;
-            int curTime = q.peek().time;
-            q.remove();
+            Pair pair = q.poll();
+            int node = pair.node;
+            int time = pair.time;
 
             for(Pair p: adj.get(node)) {
                 int adjNode = p.node;
-                int t = p.time;
+                int adjTime = p.time;
 
-                if(curTime + t < time[adjNode]) {
-                    time[adjNode] = t + curTime;
-                    q.add(new Pair(adjNode, time[adjNode]));
-                }
+                if(adjTime + time < t[adjNode]) {
+                    t[adjNode] = adjTime + time;
+                    q.add(new Pair(adjNode, t[adjNode]));
+                } 
             }
         }
         int res = 0;
-        for(int i = 1; i <= n; i++) {
-            res = Math.max(res, time[i]);
-        }
+        for(int i = 1; i <= n; i++) res = Math.max(t[i], res);
 
         return res == (int)1e9 ? -1 : res;
     }
