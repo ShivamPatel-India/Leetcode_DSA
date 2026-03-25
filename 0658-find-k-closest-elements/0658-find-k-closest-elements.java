c3352d8bcbd6dc536d5d7739ffcc31094438dd8d
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-24 19:42:06 -0700

    Time: 5 ms (56.55%), Space: 47.8 MB (75.34%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -57,8 +57,39 @@ class Solution {
         return ans;
 
     }
+    private List<Integer> binarySearchTwoPointers(int[] arr, int k, int x) {
+        int l = 0;
+        int r = arr.length - 1;
+
+        while(l < r) {
+            int m = (l + r) / 2;
+            if(arr[m] < x) l = m + 1;
+            else r = m;
+        }
+
+        l = l - 1;
+        r = l + 1;
+        while(r - l - 1 < k) {
+             if (l < 0) {
+                r++;
+            } else if (r >= arr.length) {
+                l--;
+            } else if (Math.abs(arr[l] - x) <= Math.abs(arr[r] - x)) {
+                l--;
+            } else {
+                r++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = l + 1; i < r; i++) {
+            result.add(arr[i]);
+        }
+        return result;
+    }
+    
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         // return LinearScanTwoPointers(arr, k, x);
-        return twoPointers(arr, k, x);
+        return binarySearchTwoPointers(arr, k, x);
     }
 }
