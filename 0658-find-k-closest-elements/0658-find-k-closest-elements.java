495ce6ffd7820f70bdaa657529e156b589164248
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-24 19:22:17 -0700

    Time: 5 ms (56.55%), Space: 47.7 MB (86.96%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -42,7 +42,23 @@ class Solution {
         Collections.sort(res);
         return res;
     }
+    private List<Integer> twoPointers(int[] arr, int k, int x) {
+        int l = 0;
+        int r = arr.length - 1;
+
+        while(r - l >= k) {
+            if(Math.abs(x-arr[l]) <= Math.abs(x-arr[r])) {
+                r--;
+            } else l++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i = l; i <= r; i++) ans.add(arr[i]);
+        return ans;
+
+    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        return LinearScanTwoPointers(arr, k, x);
+        // return LinearScanTwoPointers(arr, k, x);
+        return twoPointers(arr, k, x);
     }
 }
