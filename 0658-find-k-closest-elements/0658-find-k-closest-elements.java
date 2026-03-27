fbe76792df05beb3062137ffb32d53fd64373287
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-26 19:08:03 -0700

    Time: 4 ms (98.52%), Space: 47.6 MB (93.13%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,51 +1,17 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        // binary search with two pointers;
-        // first lets find out the closest (largest smaller than x) element
-        int n = arr.length;
         int l = 0;
-        int r = n - 1;
+        int r = arr.length - k; // r is last valid starting index
 
-        int ans = 0;
-        while(l <= r) {
+
+        while(l < r) {
             int m = (l + r) / 2;
-            if(arr[m] <= x) {
-                ans = m;
-                l = m + 1;
-            } else r = m - 1;
+            if(x-arr[m] > arr[m+k]-x) l = m + 1; // window too far left, slide right
+            else r = m; // window too far right, slide left
         }
 
-        int start = 0;
-        if(ans + 1 < n && Math.abs(arr[ans+1]-x) < Math.abs(arr[ans]-x)) {
-            start = ans + 1; // right neighbor is closer
-        } else start = ans;
-
-        // now ans is pointing to the closest element to the x (largest smaller in this case)
-        l = start - 1;
-        r = start + 1;
-        
         List<Integer> res = new ArrayList<>();
-        res.add(arr[start]);
-
-        while(r - l - 1 < k) {
-            if(l >= 0 && r < n) {
-                if(Math.abs(arr[r]-x) >= Math.abs(arr[l]-x)) {
-                    res.add(arr[l]);
-                    l--;
-                } else {
-                    res.add(arr[r]);
-                    r++;
-                }
-            } else if (l < 0) {
-                res.add(arr[r]);
-                r++;
-            } else {
-                res.add(arr[l]);
-                l--;
-            }
-        }
-        
-        Collections.sort(res);
+        for(int i = l; i < l + k; i++) res.add(arr[i]);
         return res;
     }
 }
