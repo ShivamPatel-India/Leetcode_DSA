7437029a21d615b4f6eabdf9831de1595b4547ea
ShivamPatel <shivampatel.sp515@gmail.com>
2026-03-29 06:14:50 -0700

    Time: 7 ms (52.42%), Space: 66 MB (78.65%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,56 +1,49 @@
-class DisjointSet {
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> rank = new ArrayList<>();
-    DisjointSet(int n) {
-        for(int i = 0 ; i <= n; i++) {
-            parent.add(i);
-            rank.add(0);
-        }
-    }
-
-    public int findUPar(int node) {
-        if(node == parent.get(node)) return node;
-        else {
-            int ulp = findUPar(parent.get(node));
-            parent.set(node, ulp);
-            return parent.get(node);
-        }
-    }
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if(ulp_u == ulp_v) return;
-
-        if(rank.get(ulp_u) > rank.get(ulp_v)) {
-            parent.set(ulp_v, ulp_u);
-        } else if (rank.get(ulp_v) > rank.get(ulp_u)) {
-            parent.set(ulp_u, ulp_v);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU+1);
-        }
-    }
-}
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-        DisjointSet ds = new DisjointSet(n);
-        int extraEdges = 0;
-        int m = connections.length;
-        for(int i = 0; i < m; i++) {
-            int u = connections[i][0];
-            int v = connections[i][1];
-            if(ds.findUPar(u) == ds.findUPar(v)) extraEdges++;
-            else ds.unionByRank(u,v);
+    class DSU {
+        int[] parent, rank;
+        public DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
         }
-        int cntC = 0;
-        for(int i = 0 ; i < n; i++) {
-            if(ds.parent.get(i) == i) cntC++;
-        }
-        int ans = cntC - 1;
-        if(extraEdges >= ans) return ans;
-        else return -1;
 
+        public int findUPar(int node) {
+            if(parent[node] == node) return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        public void union(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+            
+            if(ulp_u == ulp_v) return;
+
+            if(rank[ulp_u] < rank[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+            } else if (rank[ulp_v] < rank[ulp_u]) {
+                parent[ulp_v] = ulp_u;
+            } else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+    }
+    public int makeConnected(int n, int[][] connections) {
+        DSU ds = new DSU(n);
+        int extraEdges = 0;
+        for(int[] c: connections) {
+            if(ds.findUPar(c[0]) == ds.findUPar(c[1])) extraEdges++;
+            else ds.union(c[0],c[1]);
+        }
+
+        int components = 0;
+        for(int i = 0; i < n; i++) {
+            if(ds.parent[i] == i) components++;
+        }
+
+        return extraEdges >= (components - 1) ? (components - 1) : -1;
     }
 }
