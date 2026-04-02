061dab81541ddae87af1a2493d7bb69a2c27115e
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-02 02:50:53 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Articulation Point - I/articulation-point-i.java
+++ Difficulty: Hard/Articulation Point - I/articulation-point-i.java
@@ -1,40 +1,37 @@
 class Solution {
-    private void DFS(int node, int parent, ArrayList<ArrayList<Integer>> adj, int[] disc, int[] low, boolean[] vis, int timer, boolean[] isArticulationPoint) {
+    private void DFS(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] vis, boolean[] isAP, 
+    int[] disc, int[] low, int timer) {
         vis[node] = true;
         disc[node] = timer;
         low[node] = timer;
         int child = 0;
-        for(int neighbour: adj.get(node)) {
-            if(neighbour == parent) continue;
-            if(vis[neighbour]) {
-                low[node] = Math.min(low[node], disc[neighbour]);
+        
+        for(int neigh: adj.get(node)) {
+            if(neigh == parent) continue;
+            if(vis[neigh]) {
+                low[node] = Math.min(low[node], disc[neigh]);
             } else {
-                child += 1;
-                timer += 1;
-                DFS(neighbour, node, adj, disc, low, vis, timer, isArticulationPoint);
-                if(disc[node] <= low[neighbour] && parent != -1) {
-                    isArticulationPoint[node] = true;
-                }
-                low[node] = Math.min(low[node], low[neighbour]);
+                child++;
+                timer++;
+                DFS(neigh, node, adj, vis, isAP, disc, low, timer);
+                if(disc[node] <= low[neigh] && parent != -1) isAP[node] = true;
+                low[node] = Math.min(low[node], low[neigh]);
             }
         }
-        if(child > 1 && parent == -1) isArticulationPoint[node] = true;
-        
+        if(child > 1 && parent == -1) isAP[node] = true;
+
     }
     public ArrayList<Integer> articulationPoints(int V,
                                                  ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> ans = new ArrayList<>();
         boolean[] vis = new boolean[V];
-        boolean[] isArticulationPoint = new boolean[V];
+        boolean[] isAP = new boolean[V];
         int[] disc = new int[V];
         int[] low = new int[V];
         int timer = 0;
-        DFS(0, -1, adj, disc, low, vis, timer, isArticulationPoint);
-        for(int i = 0; i < V; i++) {
-            if(isArticulationPoint[i] == true) ans.add(i);
-        }
+        DFS(0, -1, adj, vis, isAP, disc, low, timer);
+        for(int i = 0; i < V; i++) if(isAP[i]) ans.add(i);
         if(ans.size() == 0) ans.add(-1);
         return ans;
-        
     }
 }
