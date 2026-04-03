13cbd77e39b555d5898bb650240b6cac2a024276
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-03 16:37:19 -0700

    Time: 6 ms (80.22%), Space: 47.3 MB (19.42%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,28 +1,19 @@
 class Solution {
-    private void DFS(String airport, Map<String, List<String>> map, List<String> ans) {
-        List<String> destinations = map.get(airport);
-        while(destinations != null && !destinations.isEmpty()) {
-            String next = destinations.remove(0);
-            DFS(next, map, ans);
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        for(List<String> ticket: tickets) {
+            graph.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
+        }
+
+        List<String> ans = new ArrayList<>();
+        DFS("JFK", graph, ans);
+        return ans;
+    }
+    private void DFS(String airport, Map<String, PriorityQueue<String>> graph, List<String> ans) {
+        PriorityQueue<String> pq = graph.get(airport);
+        while(pq != null && !pq.isEmpty()) {
+            DFS(pq.poll(), graph, ans);
         }
         ans.addFirst(airport);
-    }
-    public List<String> findItinerary(List<List<String>> tickets) {
-        // create a map to store the list of destinations for each departure airport
-        Map<String, List<String>> map = new HashMap<>();
-        for(List<String> ticket: tickets) {
-            String to = ticket.get(1);
-            String from = ticket.get(0);
-            map.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
-        }
-
-        // sort all values of every key in lexical order
-        for(List<String> destinations: map.values()) {
-            Collections.sort(destinations);
-        }
-
-        List<String> itinerary = new ArrayList<>();
-        DFS("JFK", map, itinerary);
-        return itinerary;
     }
 }
