33fdf557158e033588986c1712c2aab10d9b8f10
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-04 17:06:18 -0700

    Time: 17 ms (5.1%), Space: 50.3 MB (8.43%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        // Sort intervals based on start time
+        Arrays.sort(
+            intervals,
+            (a, b) -> Integer.compare(a[0], b[0])
+        );
+
+        // List to store merged intervals
+        List<List<Integer>> merged = new ArrayList<>();
+
+        // Traverse through all intervals
+        for (int[] interval : intervals) {
+            // If merged list is empty or no overlap
+            if (
+                merged.isEmpty() ||
+                merged.get(merged.size() - 1).get(1) < interval[0]
+            ) {
+                // Add current interval as a new block
+                merged.add(
+                    Arrays.asList(interval[0], interval[1])
+                );
+            } else {
+                // Overlapping: update end to max of both
+                int last = merged.size() - 1;
+                int maxEnd = Math.max(
+                    merged.get(last).get(1),
+                    interval[1]
+                );
+                merged.get(last).set(1, maxEnd);
+            }
+        }
+
+        int[][] ans = merged.stream()
+                    .map(innerList -> innerList.stream().mapToInt(Integer::intValue).toArray())
+                    .toArray(int[][]::new);
+        return ans;
+
+    }
+}
