2e13ba5481cc9e2addd8492808947493e96c3e1a
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-06 13:22:41 -0700

    Time: 4 ms (98.49%), Space: 47.2 MB (93.28%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,17 +1,17 @@
 class Solution {
     private int[] dx = {0, 0, -1, 1};
     private int[] dy = {-1, 1, 0, 0};
-    private void DFS(int x, int y, boolean[][] vis, int[][] heights, int min) {
-        if(min > heights[x][y]) return;
-        vis[x][y] = true;
-        min = heights[x][y];
+    private void DFS(int i, int j, int[][] vis, int[][] heights, int prevHeight, int ocean) {
+        if(heights[i][j] < prevHeight) return;
+        prevHeight = heights[i][j];
+        vis[i][j] |= ocean;
+        
+        for(int k = 0; k < 4; k++) {
+            int i_ = i + dx[k];
+            int j_ = j + dy[k];
 
-        for(int i = 0; i < 4; i++) {
-            int x_ = x + dx[i];
-            int y_ = y + dy[i];
-
-            if(x_ >= 0 && y_ >= 0 && x_ < heights.length && y_ < heights[0].length && !vis[x_][y_]) {
-                DFS(x_, y_, vis, heights, min);
+            if(i_ >= 0 && j_ >= 0 && i_ < heights.length && j_ < heights[0].length && (vis[i_][j_] & ocean) == 0) {
+                DFS(i_, j_, vis, heights, prevHeight, ocean);
             }
         }
     }
@@ -19,28 +19,22 @@ class Solution {
         int m = heights.length;
         int n = heights[0].length;
 
-        boolean[][] pacificVis = new boolean[m][n];
-        boolean[][] atlanticVis = new boolean[m][n];
+        int[][] vis = new int[m][n];
 
+        // top and bottom row
         for(int i = 0; i < n; i++) {
-            //Top row - pacific
-            DFS(0, i, pacificVis, heights, Integer.MIN_VALUE);
-            // Bottom row - atlantic
-            DFS(m-1, i, atlanticVis, heights, Integer.MIN_VALUE);
+            DFS(0, i, vis, heights, Integer.MIN_VALUE, 1); // 1 - pacific
+            DFS(m-1, i, vis, heights, Integer.MIN_VALUE, 2); // 2 - atlantic
         }
+        // left and right column
         for(int i = 0; i < m; i++) {
-            // Left column - pacific
-            DFS(i, 0, pacificVis, heights, Integer.MIN_VALUE);
-            // Right column - atlantic
-            DFS(i, n-1, atlanticVis, heights, Integer.MIN_VALUE);
+            DFS(i, 0, vis, heights, Integer.MIN_VALUE, 1);
+            DFS(i, n-1, vis, heights, Integer.MIN_VALUE, 2);
         }
-
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(pacificVis[i][j] && atlanticVis[i][j]) {
-                    ans.add(Arrays.asList(i,j));
-                }
+                if(vis[i][j] == 3) ans.add(Arrays.asList(i,j));
             }
         }
         return ans;
