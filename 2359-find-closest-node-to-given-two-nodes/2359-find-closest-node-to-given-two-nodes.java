ae3e0d767df073274f2b5c9975050e4dd5452043
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-09 15:59:24 -0700

    Time: 10 ms (98.35%), Space: 102.7 MB (55.12%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,44 +1,26 @@
 class Solution {
-    private void BFS(int src, int[] dist, List<List<Integer>> adj) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{src, 0});
-        dist[src] = 0;
+    private int[] getDist(int[] edges, int node) {
+        int n = edges.length;
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
 
-        while(!q.isEmpty()) {
-            int[] node = q.poll();
-
-            for(int neigh: adj.get(node[0])) {
-                if(dist[neigh] > 1 + node[1]) {
-                    dist[neigh] = 1 + node[1];
-                    q.add(new int[]{neigh, dist[neigh]});
-                }
-            }
+        int current = node;
+        int d = 0;
+        while(current != -1 && dist[current] == -1) {
+            dist[current] = d++;
+            current = edges[current];
         }
+        return dist;
     }
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        
-        // prepare adjacency list
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for(int i = 0; i < n; i++) {
-            if(edges[i] == -1) continue;
-            adj.get(i).add(edges[i]);
-        }
-
-        // we will find the shortest distance taken to reach all the node from node1 and node2
-        int[] dist1 = new int[n];
-        Arrays.fill(dist1, (int)1e9);
-        int[] dist2 = new int[n];
-        Arrays.fill(dist2, (int)1e9);
-
-        BFS(node1, dist1, adj);
-        BFS(node2, dist2, adj);
+        int[] dist1 = getDist(edges, node1);
+        int[] dist2 = getDist(edges, node2);
 
         int minDist = Integer.MAX_VALUE;
         int ans = -1;
         for(int i = 0; i < n; i++) {
-            if(dist1[i] == (int)1e9 || dist2[i] == (int)1e9) continue;
+            if(dist1[i] == -1 || dist2[i] == -1) continue;
             int max = Math.max(dist1[i], dist2[i]);
             if(max < minDist) {
                 minDist = max;
