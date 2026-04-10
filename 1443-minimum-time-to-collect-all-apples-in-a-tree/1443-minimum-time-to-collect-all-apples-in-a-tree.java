acc4da77b1a9029e288e9fa19c6529a814219bed
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-10 02:34:20 -0700

    Time: 17 ms (93.45%), Space: 86 MB (77.8%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,28 +1,33 @@
 class Solution {
-    private int DFS(int node, int parent, List<List<Integer>> adj, List<Boolean> hasApple) {
+    private int DFS(int curr, int parent, List<List<Integer>> adj, boolean[] hasApple) {
         int time = 0;
 
-        for(int child: adj.get(node)) {
-            // to avoid going back up
+        for(int child: adj.get(curr)) {
             if(child == parent) continue;
 
-            int timeFromChild = DFS(child, node, adj, hasApple);
+            int timeFromChild = DFS(child, curr, adj, hasApple);
 
-            if(hasApple.get(child) || timeFromChild > 0) {
+            // visit this child's subtree only if it has an apple or child itself has apple
+            if(timeFromChild > 0 || hasApple[child]) {
                 time += 2 + timeFromChild;
             }
         }
+
         return time;
     }
+
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
         for(int[] e: edges) {
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
 
-        return DFS(0, -1, adj, hasApple);
+        // convert List<Boolean> to boolean[]
+        boolean[] apples = new boolean[n];
+        for(int i = 0; i < n; i++) apples[i] = hasApple.get(i);
+
+        return DFS(0, -1, adj, apples);
     }
 }
