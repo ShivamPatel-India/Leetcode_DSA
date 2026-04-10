497621a62a03bb2d8623d029065f9f8f1ac1ee54
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-09 22:57:11 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.java
@@ -1,39 +1,30 @@
 class Solution {
     public int[] shortestPath(int V, int[][] edges, int src) {
-        int[] dist = new int[V];
-        for(int i = 0; i < V; i++) {
-            dist[i] = (int)1e9;
-        }
-        
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < V; i++) {
-            adj.add(new ArrayList<>());
+        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for(int[] e: edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
         }
         
-        for(int[] edge: edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
-        }
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int)1e9);
         
         Queue<Integer> q = new LinkedList<>();
-        
-        dist[src] = 0;
         q.add(src);
+        dist[src] = 0;
         
         while(!q.isEmpty()) {
             int node = q.poll();
             
             for(int adjNode: adj.get(node)) {
-                if(dist[adjNode] > dist[node] + 1) {
-                    dist[adjNode] = dist[node] + 1;
+                if(dist[adjNode] > 1 + dist[node]) {
+                    dist[adjNode] = 1 + dist[node];
                     q.add(adjNode);
                 }
             }
         }
-        
-        for(int i = 0; i < V; i++) {
-            if(dist[i] == (int)1e9) dist[i] = -1;
-        }
+        for(int i = 0; i < V; i++) if(dist[i] == (int)1e9) dist[i] = -1;
         return dist;
     }
 }
