6686c2f650dde62f9fe42ffb75e764b17673c6ed
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-11 14:57:42 -0700

    Time: 1 ms (82.87%), Space: 43 MB (9.89%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,22 +1,22 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-        String[] sArray = s.split(" ");
-        int m = pattern.length();
-        int n = sArray.length;
+    public boolean wordPattern(String pattern, String strings) {
+        String[] sArray = strings.split(" ");
+        int n = pattern.length();
+        int m = sArray.length;
         if(m != n) return false;
 
+        // we have to do char to word mapping and also the word to char mapping cz same word must not be mapped with two chars
         Map<Character, String> charToWord = new HashMap<>();
         Map<String, Character> wordToChar = new HashMap<>();
 
         for(int i = 0; i < m; i++) {
             char c = pattern.charAt(i);
-            String word = sArray[i];
-
-            if(charToWord.containsKey(c) && !charToWord.get(c).equals(word)) return false;
-            if(wordToChar.containsKey(word) && wordToChar.get(word) != c) return false;
-
-            charToWord.put(c, word);
-            wordToChar.put(word, c);
+            String s = sArray[i];
+            
+            if(charToWord.containsKey(c) && !charToWord.get(c).equals(s)) return false;
+            if(wordToChar.containsKey(s) && wordToChar.get(s) != c) return false;
+            charToWord.put(c, s);
+            wordToChar.put(s, c);
         }
         return true;
     }
