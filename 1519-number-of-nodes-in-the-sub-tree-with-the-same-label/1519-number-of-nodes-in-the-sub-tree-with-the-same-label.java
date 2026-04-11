97a35e6607c3c56ad8b8a2459edbc68335e34077
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-10 18:45:37 -0700

    Time: 55 ms (56.69%), Space: 177.9 MB (77.07%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -2,7 +2,7 @@ class Solution {
     private int[] DFS(int node, int parent, List<List<Integer>> adj, int[] ans, char[] labels) {
         int[] freq = new int[26];
         freq[labels[node]-'a']++;
-    
+
         for(int child: adj.get(node)) {
             if(child == parent) continue;
             int[] childFreq = DFS(child, node, adj, ans, labels);
@@ -14,14 +14,12 @@ class Solution {
     public int[] countSubTrees(int n, int[][] edges, String labels) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        char[] labelsArr = labels.toCharArray();
-
         for(int[] e: edges) {
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
         int[] ans = new int[n];
-        DFS(0, -1, adj, ans, labelsArr);
+        DFS(0, -1, adj, ans, labels.toCharArray());
         return ans;
     }
 }
