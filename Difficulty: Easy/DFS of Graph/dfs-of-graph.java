3cd7b5670ef1a5d620e63a79095f3397effbc060
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-12 12:54:34 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,16 +1,18 @@
 class Solution {
-    private void dfs(int node, boolean[] vis, ArrayList<Integer> ans, ArrayList<ArrayList<Integer>> adj) {
+    private void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans) {
         vis[node] = true;
         ans.add(node);
         for(int adjNode: adj.get(node)) {
-            if(!vis[adjNode]) dfs(adjNode, vis, ans, adj);
+            if(!vis[adjNode]) dfs(adjNode, adj, vis, ans);
         }
     }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-        int V = adj.size();
-        boolean[] vis = new boolean[V];
         ArrayList<Integer> ans = new ArrayList<>();
-        dfs(0, vis, ans, adj);
+        int n = adj.size();
+        boolean[] vis = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) dfs(i, adj, vis, ans);
+        }
         return ans;
     }
 }
