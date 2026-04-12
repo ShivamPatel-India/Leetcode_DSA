9967f79b808ca0a037bd0166d3e3b858dbeaec88
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-11 22:06:40 -0700

    Time: 1 ms (98.56%), Space: 45.3 MB (86.35%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,23 +1,14 @@
 class Solution {
-    private void sync(int i, int j, char[][] board) {
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '.') return;
-        board[i][j] = '.';
-        sync(i, j+1, board);
-        sync(i, j-1, board);
-        sync(i+1, j, board);
-        sync(i-1, j, board);
-    }
     public int countBattleships(char[][] board) {
-        int battleShips = 0;
         int m = board.length;
         int n = board[0].length;
-
+        int battleShips = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(board[i][j] == 'X') {
-                    battleShips++;
-                    sync(i,j,board);
-                }
+                if(board[i][j] == '.') continue;
+                if(i > 0 && board[i-1][j] == 'X') continue;
+                if(j > 0 && board[i][j-1] == 'X') continue;
+                battleShips++;
             }
         }
         return battleShips;
