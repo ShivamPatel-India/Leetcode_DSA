59e715cc5a0a0f72d579a8c2cded68b15ac274f6
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-12 18:02:15 -0700

    Time: 0 ms (100%), Space: 47.3 MB (46.53%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,52 +1,22 @@
 class Solution {
-    class DSU {
-        int[] parent;
-        int[] rank;
-
-        public DSU(int n) {
-            parent = new int[n];
-            rank = new int[n];
-            for(int i = 0; i < n; i++) {
-                parent[i] = i;
-                rank[i] = 0;
-            }
-        }
-        public int findParent(int node) {
-            if(parent[node] == node) return node;
-            return parent[node] = findParent(parent[node]);
-        }
-        public void union(int u, int v) {
-            int pu = findParent(u);
-            int pv = findParent(v);
-            if(pu == pv) return;
-            int rpu = rank[pu];
-            int rpv = rank[pv];
-            if(rpu < rpv) {
-                parent[pu] = pv;
-            } else if(rpv < rpu) {
-                parent[pv] = pu;
-            } else {
-                parent[pv] = pu;
-                rank[pu]++;
-            }
+    public void dfs(int node, int[][] isConnected, boolean[] vis) {
+        int n = isConnected.length;
+        vis[node] = true;
+        for(int i = 0; i < n; i++) {
+            if(isConnected[node][i] == 1 && !vis[i]) dfs(i, isConnected, vis);
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        List<List<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        DSU ds = new DSU(n);
-        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        int province = 0;
+        boolean[] vis = new boolean[n];
+
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1 && ds.findParent(i) != ds.findParent(j)) {
-                    ds.union(i, j);
-                }
+            if(!vis[i]) {
+                province++;
+                dfs(i, isConnected, vis);
             }
         }
-        int components = 0;
-        for(int i = 0; i < n; i++) {
-            if(ds.parent[i] == i) components++;
-        }
-        return components;
+        return province;
     }
 }
