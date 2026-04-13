037c59a302329bbde80e03f98e9fe0128362b209
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-13 08:51:29 -0700

    Time: 1 ms (39.87%), Space: 46.9 MB (48.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,24 @@
 class Solution {
-    public void dfs(int row, int col, int[][] image, int[][] ans, int[] dx, int[] dy, int initialColor, int color) {
-        ans[row][col] = color;
-        for(int i = 0; i < 4; i++) {
-            int x = dx[i] + row;
-            int y = dy[i] + col;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m = image.length;
+        int n = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        int originalColor = image[sr][sc];
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        while(!q.isEmpty()) {
+            int[] point = q.poll();
+            image[point[0]][point[1]] = color;
+            for(int i = 0; i < 4; i++) {
+                int nsr = point[0] + dx[i];
+                int nsc = point[1] + dy[i];
 
-            if(x>=0 && y >=0 && x<image.length && y<image[0].length && image[x][y] == initialColor && ans[x][y] != color) {
-                dfs(x, y, image, ans, dx, dy, initialColor, color);
+                if(nsr < 0 || nsc < 0 || nsr >= m || nsc >= n || image[nsr][nsc] != originalColor || image[nsr][nsc] == color) continue;
+                image[nsr][nsc] = color;
+                q.add(new int[]{nsr, nsc});
             }
         }
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
-        int ans[][] = image;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        dfs(sr, sc, image, ans, dx, dy, initialColor, color);
-        return ans;
+        return image;
     }
 }
