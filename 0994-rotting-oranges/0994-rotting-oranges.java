136d7f08676b1af402b04b42a4fa8e48960b8c09
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-12 21:04:32 -0700

    Time: 2 ms (89.93%), Space: 44.5 MB (17.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,41 +2,39 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
 
-        Queue<int[]> q = new LinkedList<>();
         int totalOranges = 0;
+        Queue<int[]> q = new LinkedList<>();
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 0) continue;
+                if(grid[i][j] > 0) totalOranges++;
                 if(grid[i][j] == 2) q.add(new int[]{i,j});
-                totalOranges++;
             }
         }
 
-        int explored = 0;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        int exploredOranges = 0;
         int minTime = 0;
         while(!q.isEmpty()) {
             int size = q.size();
-            explored += size;
+            exploredOranges += size;
 
             for(int i = 0; i < size; i++) {
-                int x = q.peek()[0];
-                int y = q.peek()[1];
-                q.poll();
-                for(int j = 0; j < 4; j++) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
+                int[] point = q.poll();
+                for(int k = 0; k < 4; k++) {
+                    int nx = point[0] + dx[k];
+                    int ny = point[1] + dy[k];
 
-                    if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] != 0 && grid[nx][ny] != 2) {
-                        grid[nx][ny] = 2;
-                        q.add(new int[]{nx, ny});
-                    }
-                }
+                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] == 2 || grid[nx][ny] == 0) continue;
+                    // rotting happens
+                    grid[nx][ny] = 2;
+                    q.add(new int[]{nx, ny});
+                } 
             }
-            if(q.size() != 0) minTime++;
+            // increase the time only if q is not empty that is rotting happened. If q is empty rotting didn't happen so no need to increase the time in that case.
+            if(!q.isEmpty()) minTime++;
         }
-        return explored == totalOranges ? minTime : -1;
+        return exploredOranges == totalOranges ? minTime : -1;
     }
 }
