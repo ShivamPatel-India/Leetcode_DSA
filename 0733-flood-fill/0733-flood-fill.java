bcca31361358d44c02d619672ac80b7238ac12b4
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-13 09:05:29 -0700

    Time: 0 ms (100%), Space: 46.8 MB (67.27%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,15 +1,11 @@
 class Solution {
-    public int[] dx = {-1,1,0,0};
-    public int[] dy = {0,0,-1,1};
-    public void dfs(int i, int j, int[][] image, int color, int oc) {
-        image[i][j] = color;
-        for(int k = 0; k < 4; k++) {
-            int ni = i + dx[k];
-            int nj = j + dy[k];
-
-            if(ni<0 || nj<0 || ni>=image.length || nj>=image[0].length || image[ni][nj]==color || image[ni][nj]!=oc) continue;
-            dfs(ni, nj, image, color, oc);
-        }
+    public void dfs(int ni, int nj, int[][] image, int color, int oc) {
+        if(ni<0 || nj<0 || ni>=image.length || nj>=image[0].length || image[ni][nj]==color || image[ni][nj]!=oc) return;
+        image[ni][nj] = color;
+        dfs(ni+1, nj, image, color, oc);
+        dfs(ni-1, nj, image, color, oc);
+        dfs(ni, nj+1, image, color, oc);
+        dfs(ni, nj-1, image, color, oc);
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
