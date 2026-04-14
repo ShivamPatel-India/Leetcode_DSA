d232ecf516d049d7f3b830a33251d3d798a82499
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-13 20:41:20 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,12 +1,11 @@
 class Solution {
-    public boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] pathVis) {
+    private boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] pathVis) {
         vis[node] = true;
         pathVis[node] = true;
-        
         for(int adjNode: adj.get(node)) {
             if(!vis[adjNode]) {
                 if(dfs(adjNode, adj, vis, pathVis)) return true;
-            } else if(pathVis[adjNode])return true;
+            } else if(pathVis[adjNode]) return true;
         }
         pathVis[node] = false;
         return false;
@@ -14,15 +13,12 @@ class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        for(int[] e: edges) {
+            adj.get(e[0]).add(e[1]);
+        }
         
-        for(int[] edge: edges) adj.get(edge[0]).add(edge[1]);
-        
-        // to keep track of the already visited nodes
         boolean[] vis = new boolean[V];
-        
-        // to keep track of the already visited nodes in the current path
         boolean[] pathVis = new boolean[V];
-        
         for(int i = 0; i < V; i++) {
             if(!vis[i]) {
                 if(dfs(i, adj, vis, pathVis)) return true;
