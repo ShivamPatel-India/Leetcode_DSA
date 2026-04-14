e755724a4a8e5e582ea086db93d81f2de112a440
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-13 18:08:53 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,39 +1,26 @@
 class Solution {
-    private boolean detectCycle(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
+    private boolean dfs(int node, int parent, List<List<Integer>> adj, boolean[] vis) {
         vis[node] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{node, -1});
-        
-        while(!q.isEmpty()) {
-            int currentNode = q.peek()[0];
-            int parentNode = q.peek()[1];
-            q.poll();
-            
-            for(int adjNode: adj.get(currentNode)) {
-                if(!vis[adjNode]) {
-                    vis[adjNode] = true;
-                    q.offer(new int[]{adjNode, currentNode});
-                } else if (parentNode != adjNode) {
-                    return true;
-                }
-            }
+        for(int adjNode: adj.get(node)) {
+            if(!vis[adjNode]) {
+                if(dfs(adjNode, node, adj, vis)) return true;
+            } else if (adjNode != parent) return true;
         }
         return false;
     }
     public boolean isCycle(int V, int[][] edges) {
-        // Code here
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
-        for(int[] edge: edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+        
+        for(int[] e: edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
         }
         
         boolean[] vis = new boolean[V];
-        
         for(int i = 0; i < V; i++) {
             if(!vis[i]) {
-                if(detectCycle(i, adj, vis)) return true;
+                if(dfs(i, -1, adj, vis)) return true;
             }
         }
         return false;
