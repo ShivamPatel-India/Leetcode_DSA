7b559960e3c4f1f1f05cfc53a930779058cf65c9
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-14 23:07:04 -0700

    Time: 14 ms (71.45%), Space: 49.9 MB (6.24%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,53 +1,37 @@
 class Solution {
-    public class Node {
-        int row, col, dist;
-        Node(int _row, int _col, int _dist) {
-            this.row = _row;
-            this.col = _col;
-            this.dist = _dist;
-        }
-    }
+    // at first we might think to hit the BFS from every cell which has 1 to find the nearest 0, but that would be quite inefficient O(m*n) * O(m*n)
+    // instead of that we can take all the 0 and hit the BFS from there
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
+       int m = mat.length;
+       int n = mat[0].length;
+       Queue<int[]> q = new LinkedList<>(); // [i, j, steps]
+       boolean[][] vis = new boolean[m][n];
+       int[][] dist = new int[m][n];
 
-        int[][] vis = new int[n][m];
-        int[][] dist = new int[n][m];
-
-        Queue<Node> q = new LinkedList<>();
-
-        for(int i = 0 ; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+       for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(mat[i][j] == 0) {
-                    q.add(new Node(i,j, 0));
-                    vis[i][j] = 0; // marking already visited nodes as 0
-                } else {
-                    vis[i][j] = 1;
-                }
+                    q.add(new int[]{i,j,0}); 
+                    vis[i][j] = true;
+                } else vis[i][j] = false;
             }
+       }
+       int[] dx = {-1,1,0,0};
+       int[] dy = {0,0,-1,1};
+
+       while(!q.isEmpty()) {
+        int[] point = q.poll();
+        int x = point[0], y = point[1], steps = point[2];
+        dist[x][y] = steps;
+        for(int i = 0 ; i < 4 ; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx<0 || ny<0 || nx>=m || ny>=n || vis[nx][ny]) continue;
+            vis[nx][ny] = true;
+            q.add(new int[]{nx, ny, steps+1});
         }
-
-        int[] delRow = {-1,1,0,0};
-        int[] delCol = {0,0,-1,1};
-
-        while(!q.isEmpty()) {
-            int row = q.peek().row;
-            int col = q.peek().col;
-            int steps = q.peek().dist;
-            q.remove();
-
-            dist[row][col] = steps;
-
-            for(int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==1) {
-                    vis[nrow][ncol] = 0;
-                    q.add(new Node(nrow, ncol, steps + 1));
-                }
-            }
-        }
-        return dist;
+       }
+       return dist;
     }
 }
