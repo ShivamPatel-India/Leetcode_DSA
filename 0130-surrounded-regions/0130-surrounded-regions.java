01ef93f516f5b20e3a6b510ea64a4494ab5c3b54
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-15 14:18:06 -0700

    Time: 2 ms (78.13%), Space: 47.4 MB (95.95%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,13 +3,14 @@ class Solution {
     // after that we can find other unvisited 'O' in the matrix and can say they essentially form a surrounded region and we can mark them as 'X'
     private int[] dx = {-1,1,0,0};
     private int[] dy = {0,0,-1,1};
-    private void dfs(int nx, int ny, char[][] board, boolean[][] vis) {
-        if(nx<0 || ny<0 || nx>=board.length || ny>=board[0].length || vis[nx][ny] || board[nx][ny] == 'X') return;
-        vis[nx][ny] = true;
-        dfs(nx+1, ny, board, vis);
-        dfs(nx-1, ny, board, vis);
-        dfs(nx, ny+1, board, vis);
-        dfs(nx, ny-1, board, vis);
+    private void dfs(int x, int y, char[][] board, boolean[][] vis) {
+        vis[x][y] = true;
+        for(int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if(nx<0 || ny<0 || nx>=board.length || ny>=board[0].length || vis[nx][ny] || board[nx][ny] == 'X') continue;
+            dfs(nx, ny, board, vis);
+        }
     }
     public void solve(char[][] board) {
         int m = board.length;
