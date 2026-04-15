c1071d19eac139b53582fdfffaff51f5733e83cb
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-14 23:16:27 -0700

    Time: 13 ms (92.89%), Space: 49.7 MB (16.59%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -5,15 +5,14 @@ class Solution {
        int m = mat.length;
        int n = mat[0].length;
        Queue<int[]> q = new LinkedList<>(); // [i, j, steps]
-       boolean[][] vis = new boolean[m][n];
        int[][] dist = new int[m][n];
 
        for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(mat[i][j] == 0) {
                     q.add(new int[]{i,j,0}); 
-                    vis[i][j] = true;
-                } else vis[i][j] = false;
+                    dist[i][j] = 0;
+                } else dist[i][j] = -1;
             }
        }
        int[] dx = {-1,1,0,0};
@@ -22,13 +21,13 @@ class Solution {
        while(!q.isEmpty()) {
         int[] point = q.poll();
         int x = point[0], y = point[1], steps = point[2];
-        dist[x][y] = steps;
+        
         for(int i = 0 ; i < 4 ; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(nx<0 || ny<0 || nx>=m || ny>=n || vis[nx][ny]) continue;
-            vis[nx][ny] = true;
+            if(nx<0 || ny<0 || nx>=m || ny>=n || dist[nx][ny]!=-1) continue;
+            dist[nx][ny] = 1 + steps;
             q.add(new int[]{nx, ny, steps+1});
         }
        }
