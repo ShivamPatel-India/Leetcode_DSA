007ca44a3065b955a73c5ef59f2d79b5807ee6a2
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-15 14:16:15 -0700

    Time: 2 ms (78.13%), Space: 47.8 MB (61.3%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -3,30 +3,30 @@ class Solution {
     // after that we can find other unvisited 'O' in the matrix and can say they essentially form a surrounded region and we can mark them as 'X'
     private int[] dx = {-1,1,0,0};
     private int[] dy = {0,0,-1,1};
-    private void dfs(int x, int y, char[][] board, boolean[][] vis) {
-        vis[x][y] = true;
-        for(int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if(nx<0 || ny<0 || nx>=board.length || ny>=board[0].length || vis[nx][ny] || board[nx][ny] == 'X') continue;
-            dfs(nx, ny, board, vis);
-        }
+    private void dfs(int nx, int ny, char[][] board, boolean[][] vis) {
+        if(nx<0 || ny<0 || nx>=board.length || ny>=board[0].length || vis[nx][ny] || board[nx][ny] == 'X') return;
+        vis[nx][ny] = true;
+        dfs(nx+1, ny, board, vis);
+        dfs(nx-1, ny, board, vis);
+        dfs(nx, ny+1, board, vis);
+        dfs(nx, ny-1, board, vis);
     }
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
         boolean[][] vis = new boolean[m][n];
 
-        // first row
-        for(int k = 0; k < n; k++) if(board[0][k] == 'O' && !vis[0][k]) dfs(0,k,board,vis);
-        // last row
-        for(int k = 0; k < n; k++) if(board[m-1][k] == 'O' && !vis[m-1][k]) dfs(m-1,k,board,vis);
+        // first row - last row
+        for(int k = 0; k < n; k++) {
+            if(board[0][k] == 'O' && !vis[0][k]) dfs(0,k,board,vis);
+            if(board[m-1][k] == 'O' && !vis[m-1][k]) dfs(m-1,k,board,vis);
+        }
 
-        // fist column
-        for(int k = 0; k < m; k++) if(board[k][0] == 'O' && !vis[k][0]) dfs(k,0,board,vis);
-        // last column
-        for(int k = 0; k < m; k++) if(board[k][n-1] == 'O' && !vis[k][n-1]) dfs(k,n-1,board,vis);
+        // fist column - last column
+        for(int k = 0; k < m; k++) {
+            if(board[k][0] == 'O' && !vis[k][0]) dfs(k,0,board,vis);
+            if(board[k][n-1] == 'O' && !vis[k][n-1]) dfs(k,n-1,board,vis);
+        }
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
