c96780e45d932d6da300424ccb9c3e612aec90e5
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-15 17:24:21 -0700

    Time: 100 ms (28.67%), Space: 48.7 MB (36.67%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -2,41 +2,38 @@ class Solution {
     class Pair {
         String word;
         int steps;
-        Pair(String _word, int _steps) {
-            this.word = _word;
-            this.steps = _steps;
+        Pair(String word, int steps) {
+            this.word = word;
+            this.steps = steps;
         }
     }
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int n = beginWord.length();
         Queue<Pair> q = new LinkedList<>();
         Set<String> s = new HashSet<>();
-
-        for(String word: wordList) {
-            s.add(word);
-        }
+        for(String word: wordList) s.add(word);
         s.remove(beginWord);
         q.add(new Pair(beginWord, 1));
 
         while(!q.isEmpty()) {
-            String word = q.peek().word;
-            int steps = q.peek().steps;
-            q.remove();
+            Pair p = q.poll();
+            String word = p.word;
+            int steps = p.steps;
 
             if(word.equals(endWord)) return steps;
-
-            // otherwise try and generate all possible words out of current word and check if any of them present in set then add it to the queue with steps + 1
-            for(int i = 0; i < word.length(); i++) {
+            for(int i = 0; i < n; i++) {
                 for(char ch = 'a'; ch <= 'z'; ch++) {
                     char[] charArray = word.toCharArray();
                     charArray[i] = ch;
                     String newWord = new String(charArray);
+
                     if(s.contains(newWord)) {
                         s.remove(newWord);
-                        q.add(new Pair(newWord, steps + 1));
+                        q.add(new Pair(newWord, steps+1));
                     }
                 }
             }
         }
         return 0;
-   }
+    }
 }
