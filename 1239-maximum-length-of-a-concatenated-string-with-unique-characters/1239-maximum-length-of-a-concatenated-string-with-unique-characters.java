a4a0c11206c73778f4e286adfed21a779767434e
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-16 23:41:19 -0700

    Time: 15 ms (41.07%), Space: 50 MB (9.29%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,4 +1,5 @@
 class Solution {
+    public Map<String, Integer> map= new HashMap<>();
     private boolean hasDuplicate(String u, String v) {
         int[] freq = new int[26];
         for(char ch: u.toCharArray()) {
@@ -7,12 +8,13 @@ class Solution {
         }
         for(char ch: v.toCharArray()) {
             if(freq[ch-'a'] > 0) return true;
+            freq[ch-'a']++;
         }
         return false;
     }
     private int solve(int i, List<String> arr, String temp, int n) {
         if(i >= n) return temp.length();
-
+        if(map.containsKey(temp)) return map.get(temp);
         int include = 0;
         int exclude = 0;
         if(hasDuplicate(arr.get(i), temp)) {
@@ -21,7 +23,9 @@ class Solution {
             exclude = solve(i+1, arr, temp, n);
             include = solve(i+1, arr, temp + arr.get(i), n);
         }
-        return Math.max(include, exclude);
+        int result = Math.max(include, exclude);
+        map.put(temp, result);
+        return result;
     }
     public int maxLength(List<String> arr) {
         int n = arr.size();
