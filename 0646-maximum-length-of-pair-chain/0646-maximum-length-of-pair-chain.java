6731ec249472d564cb5eb07071295903b17cddfc
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-21 00:21:21 -0700

    Time: 30 ms (30.51%), Space: 47 MB (53.88%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -15,11 +15,26 @@ class Solution {
         if(pi != -1) return dp[i][pi] = Math.max(take, skip);
         return Math.max(take, skip);
     }
+    private int tabulation(int[][] pairs) {
+        int maxLIS = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(pairs[i][0] > pairs[j][1]) {
+                    dp[i] = 1 + dp[j];
+                    maxLIS = Math.max(dp[i], maxLIS);
+                }
+            }
+        }
+        return maxLIS;
+    }
     public int findLongestChain(int[][] pairs) {
         n = pairs.length;
-        dp = new int[n+1][n+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
+        // dp = new int[n+1][n+1];
+        // for(int[] row: dp) Arrays.fill(row, -1);
         Arrays.sort(pairs, (a,b) -> a[0] - b[0]);
-        return solve(0, pairs, -1);
+        // return solve(0, pairs, -1);
+        return tabulation(pairs);
     }
 }
