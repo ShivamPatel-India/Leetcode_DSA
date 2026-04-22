5a32279460b6e80c40a5c26a9b4acc8f8cd44697
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-21 21:16:06 -0700

    Time: 48 ms (54.16%), Space: 45.6 MB (99.51%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,38 +1,33 @@
 class Solution {
-    private int n;
-    private int[][] dp;
     private boolean isPredecessor(String a, String b) {
         int alen = a.length();
         int blen = b.length();
-        if(blen + 1 != alen) return false;
-        
-        int first = 0;
-        int second = 0;
-        while(first < alen) {
-            if(second < blen && a.charAt(first) == b.charAt(second)) {
-                first++;
-                second++;
-            } else first++;
+        if(alen != 1 + blen) return false;
+        int aPtr = 0;
+        int bPtr = 0;
+        while(aPtr < alen) {
+            if(bPtr < blen && a.charAt(aPtr) == b.charAt(bPtr)) {
+                aPtr++;
+                bPtr++;
+            } else aPtr++;
         }
-        return first == alen && second == blen;
-    }
-    private int solve(int i, String[] words, int pi) {
-        if(i >= n) return 0;
-
-        if(pi != -1 && dp[i][pi] != -1) return dp[i][pi];
-        int take = 0;
-        if(pi == -1 || isPredecessor(words[i], words[pi])) {
-            take = 1 + solve(i+1, words, i);
-        } 
-        int skip = solve(i+1, words, pi);
-        if(pi != -1) dp[i][pi] = Math.max(take, skip);
-        return Math.max(take, skip);
+        return aPtr == alen && bPtr == blen;
     }
     public int longestStrChain(String[] words) {
-        n = words.length;
-        dp = new int[n+1][n+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        Arrays.sort(words, (a, b) -> a.length()-b.length());
-        return solve(0, words, -1);
+        // tabulation approach
+        int n = words.length;
+        Arrays.sort(words, (a,b) -> a.length() - b.length());
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int ans = 1;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(isPredecessor(words[i], words[j])) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    ans = Math.max(ans, dp[i]);
+                }
+            }
+        }
+        return ans;
     }
 }
