6213850dcdf37372c414e3e8c27bad388595100c
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-24 22:24:49 -0700

    Time: 16 ms (99.84%), Space: 45.2 MB (60.21%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,41 +1,33 @@
 class Solution {
-    public List<Integer> printLDS(int[] nums) {
-        Arrays.sort(nums);
+    public List<Integer> largestDivisibleSubset(int[] nums) {
         int n = nums.length;
-
+        Arrays.sort(nums);
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
-        int[] hash = new int[n];
+        int[] prev_index = new int[n];
+        Arrays.fill(prev_index, -1);
 
+        int maxL = 1;
+        int last_chosen_index = 0;
         for(int i = 1; i < n; i++) {
-            hash[i] = i;
-            for(int prev = 0; prev < i; prev++) {
-                if(nums[i] % nums[prev] ==0  && 1 + dp[prev] > dp[i]) {
-                    dp[i] = 1 + dp[prev];
-                    hash[i] = prev;
+            for(int j = 0; j < i; j++) {
+                if(nums[i] % nums[j] == 0) {
+                    if(dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        prev_index[i] = j;
+                    }
+                    if(dp[i] > maxL) {
+                        maxL = dp[i];
+                        last_chosen_index = i;
+                    }
                 }
             }
         }
-        int lastIndex = -1;
-        int ans = -1;
-        for(int i = 0; i < n; i++) {
-            if(ans < dp[i]) {
-                ans = dp[i];
-                lastIndex = i;
-            }
+        List<Integer> ans = new ArrayList<>();
+        while(last_chosen_index != -1) {
+            ans.add(nums[last_chosen_index]);
+            last_chosen_index = prev_index[last_chosen_index];
         }
-        List<Integer> temp = new ArrayList<>();
-        temp.add(nums[lastIndex]);
-
-        while(hash[lastIndex] != lastIndex) {
-            lastIndex = hash[lastIndex];
-            temp.add(nums[lastIndex]);
-        }
-        Collections.sort(temp);
-        return temp;
-    }
-
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        return printLDS(nums);
+        return ans;
     }
 }
