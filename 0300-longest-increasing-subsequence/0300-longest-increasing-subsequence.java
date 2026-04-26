f60c319282b92c3bf7c21aa13f8fd6e86d1d0e31
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-26 01:47:29 -0700

    Time: 6 ms (86.54%), Space: 46.2 MB (55.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,27 +2,29 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         List<Integer> sorted = new ArrayList<>();
-
-        for(int i = 0; i < n; i++) {
-            int index = binarySearch(nums[i], sorted);
-
-            if(index == sorted.size()) sorted.add(nums[i]);
-            else sorted.set(index, nums[i]);
+        sorted.add(nums[0]);
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > sorted.get(sorted.size()-1)) sorted.add(nums[i]);
+            else {
+                int index = lowerBound(sorted, nums[i]);
+                sorted.set(index, nums[i]);
+            }
         }
         return sorted.size();
     }
-    private int binarySearch(int target, List<Integer> sorted) {
-        int left = 0; int right = sorted.size();
+    private int lowerBound(List<Integer> sorted, int target) {
+        int left = 0;
+        int right = sorted.size();
         int result = sorted.size();
 
         while(left < right) {
-            int mid = left + (right-left) / 2;
-            if(sorted.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
+            int mid = left + (right - left) / 2;
+
+            // we need to find the smallest greater or equal element
+            if(sorted.get(mid) >= target) {
                 result = mid;
-            }
+                right = mid;
+            } else left = mid + 1;
         }
         return result;
     }
