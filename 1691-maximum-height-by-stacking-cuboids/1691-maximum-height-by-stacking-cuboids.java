6aba39033371fe573ad10e5f6970dd3560d3b9b1
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-26 21:08:29 -0700

    Time: 7 ms (96.95%), Space: 45.1 MB (13.71%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.java
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.java
@@ -1,29 +1,35 @@
 class Solution {
+    private int[][] memo;
     public int maxHeight(int[][] cuboids) {
-        int n = cuboids.length; // total cuboids
+        // sort dimensions of each cuboid w<=l<=h (height maximum), [w, l, h]
         for(int[] cuboid: cuboids) Arrays.sort(cuboid);
-
-        Arrays.sort(cuboids, (a,b) -> {
-            if(a[0] != b[0]) return b[0] - a[0];
-            if(a[1] != b[1]) return b[1] - a[1];
-            return b[2]-a[2];
+        
+        // now sort the whole set of cuboids
+        Arrays.sort(cuboids, (a, b) -> {
+            if(a[0] != b[0]) return a[0]-b[0];
+            if(a[1] != b[1]) return a[1]-b[1];
+            return a[2]-b[2];
         });
 
-        int[] dp = new int[n];
-        int max = 0;
+        int n = cuboids.length;
+        memo = new int[n+1][n+1]; // current cube index, previous cube index
+        for(int[] row: memo) Arrays.fill(row, -1);
 
-        for(int i = 0; i < n; i++) {
-            dp[i] = cuboids[i][2];
-            for(int j = 0; j < i; j++) {
-                if(canPlace(cuboids[i], cuboids[j])) {
-                    dp[i] = Math.max(dp[i], dp[j] + cuboids[i][2]);
-                }
-            }
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+        return solve(0, -1, cuboids);
     }
-    private boolean canPlace(int[]a, int[]b) {
-        return a[0]<=b[0] && a[1]<=b[1] && a[2]<=b[2];
+    private int solve(int i, int prev, int[][] cuboids) {
+        if(i >= cuboids.length) return 0;
+        if(prev != -1 && memo[i][prev] != -1) return memo[i][prev];
+
+        // take
+        int take = 0;
+        if(prev == -1 || (cuboids[i][0] >= cuboids[prev][0] &&
+                        cuboids[i][1] >= cuboids[prev][1] &&
+                        cuboids[i][2] >= cuboids[prev][2])) {
+                            take = cuboids[i][2] + solve(i+1, i, cuboids);
+                        }
+        int skip = solve(i+1, prev, cuboids);
+        if(prev != -1) memo[i][prev] = Math.max(take, skip);
+        return Math.max(take, skip);
     }
 }
