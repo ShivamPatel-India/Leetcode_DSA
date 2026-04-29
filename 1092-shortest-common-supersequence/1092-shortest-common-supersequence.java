ccd27c42b7cb26a700720640fb80ed03de2b5d9a
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-29 14:00:11 -0700

    Time: 23 ms (71.68%), Space: 54.3 MB (8.49%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -6,8 +6,10 @@ class Solution {
 
         for(int i = 0 ; i <= m; i++) {
             for(int j = 0; j <= n; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = i+j;
+                if(i == 0) {
+                    dp[i][j] = j;
+                } else if(j == 0) {
+                    dp[i][j] = i;
                 } else if(s1.charAt(i-1) == s2.charAt(j-1)) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
