b6c54e7885c9df3380469c1e9918ce8e6db3f507
ShivamPatel <shivampatel.sp515@gmail.com>
2026-05-01 12:55:26 -0700

    Time: 14 ms (92.94%), Space: 43.5 MB (69.61%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,33 +1,24 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n+1][n+1];
-        int maxlen = Integer.MIN_VALUE;
-        int sp = 0;
-
-        for(int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            maxlen = 1;
-        }
-
-        for(int L = 2; L <= n; L++) {
-            for(int i = 0; i+L-1 < n; i++) {
-                int j = i+L-1;
-
-                if(s.charAt(i) == s.charAt(j) && L == 2) {
-                    maxlen = 2;
-                    sp = i;
-                    dp[i][j] = true;
-                } else if((s.charAt(i) == s.charAt(j)) && (dp[i+1][j-1] == true)) {
-                    dp[i][j] = true;
-                    if(j-i+1 > maxlen) {
-                        maxlen = j-i+1;
-                        sp = i;
-                    }
-                } else {
-                    dp[i][j] = false;
-                }
+    private int n;
+    private int maxlen;
+    private int sp;
+    private void check(int i, int j, String s) {
+        while(i >= 0&& j < n && s.charAt(i) == s.charAt(j)) {
+            if(j-i+1 > maxlen) {
+                maxlen = j-i+1;
+                sp = i;
             }
+            i--;
+            j++;
+        }
+    }
+    public String longestPalindrome(String s) {
+        n = s.length();
+        maxlen = Integer.MIN_VALUE;
+        sp = 0;
+        for(int i = 0; i < n; i++) {
+            check(i,i, s);
+            check(i, i+1, s);
         }
         return s.substring(sp, sp+maxlen);
     }
