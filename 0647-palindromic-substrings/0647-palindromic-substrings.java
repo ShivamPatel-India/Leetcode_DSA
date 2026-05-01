b678dda812ba8f13afc2d53d1ee93c13142bb265
ShivamPatel <shivampatel.sp515@gmail.com>
2026-04-30 18:13:00 -0700

    Time: 22 ms (22.88%), Space: 46.5 MB (22.29%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,24 +1,20 @@
 class Solution {
-    private int n;
-    private int[][] dp;
-    private int check(String s, int i, int j) {
-        if(i > j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = check(s, i+1, j-1);
-        } 
-        return dp[i][j] = 0;
-    }
     public int countSubstrings(String s) {
-        n = s.length();
-        dp = new int[n+1][n+1];
-        for(int[] row: dp) Arrays.fill(row, -1);
+        int n = s.length();
         int count = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(check(s, i, j) == 1) {
-                    count++;
+        boolean[][] dp = new boolean[n+1][n+1];
+
+        for(int L = 1; L <= n; L++) {
+            for(int i = 0; i+L-1 < n; i++) {
+                int j = i + L - 1;
+
+                if(i==j) dp[i][j] = true;
+                else if(i+1 == j) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
                 }
+                if(dp[i][j] == true) count++;
             }
         }
         return count;
