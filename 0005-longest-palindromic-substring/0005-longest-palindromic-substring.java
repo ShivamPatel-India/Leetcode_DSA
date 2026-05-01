102f9521e7a393c30a700d2083c6ca20fca1d817
ShivamPatel <shivampatel.sp515@gmail.com>
2026-05-01 13:00:19 -0700

    Time: 13 ms (95.98%), Space: 43.7 MB (59.6%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,10 +1,10 @@
 class Solution {
     private int n;
-    private int maxlen;
     private int sp;
+    private int maxlen;
     private void check(int i, int j, String s) {
-        while(i >= 0&& j < n && s.charAt(i) == s.charAt(j)) {
-            if(j-i+1 > maxlen) {
+        while(i >= 0 && j < n && s.charAt(i) == s.charAt(j)) {
+            if(maxlen < j-i+1) {
                 maxlen = j-i+1;
                 sp = i;
             }
@@ -17,7 +17,7 @@ class Solution {
         maxlen = Integer.MIN_VALUE;
         sp = 0;
         for(int i = 0; i < n; i++) {
-            check(i,i, s);
+            check(i, i, s);
             check(i, i+1, s);
         }
         return s.substring(sp, sp+maxlen);
