fc3a2ea173691d82b49f45e4da850981d4958a46
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-13 02:22:38 -0700

    Time: 0 ms (100%), Space: 42 MB (73.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,25 @@
 class Solution {
-    public int solve(int currentStep, int n, int[] dp) {
-        if(currentStep > n) return 0;
-        if(currentStep == n) return 1;
-        if(dp[currentStep] != -1) return dp[currentStep];
-        return dp[currentStep] = solve(currentStep + 1, n, dp) + solve(currentStep + 2, n, dp);
-    }
+    // public int solve(int currentStep, int n, int[] dp) {
+    //     if(currentStep > n) return 0;
+    //     if(currentStep == n) return 1;
+    //     if(dp[currentStep] != -1) return dp[currentStep];
+    //     return dp[currentStep] = solve(currentStep + 1, n, dp) + solve(currentStep + 2, n, dp);
+    // }
     public int climbStairs(int n) {
+        // Memoization - goint fro 0th stair to last stair (top-down on recursive tree)
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return solve(0, n, dp);
+
+        // Tabulation - going from last stair to 0th stair (bottom-up on recursive tree)
+        if(n <= 1) return 1;
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(0, n, dp);
+        dp[n] = 1; // one-way to be on the last stair
+        dp[n-1] = 1; // one-way to go on second last stair from last stair
+
+        for(int i = n-2; i >= 0; i--) {
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
 }
