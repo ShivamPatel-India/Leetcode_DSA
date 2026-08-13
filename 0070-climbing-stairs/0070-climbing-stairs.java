6a8ef6e0d1e26781f82add98fd8830f2508ff44e
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-13 02:10:13 -0700

    Time: 0 ms (100%), Space: 42.2 MB (36.49%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,44 +1,13 @@
-import java.util.*;
-
 class Solution {
-    public int recursive(int n) {
-        // top-down approach
-        if(n == 0 || n == 1) return 1;
-        return recursive(n-1) + recursive(n-2);
-    }
-    public int memoized(int n, int[] dp) {
-        // top-down approach with memoization
-        if(n == 0 || n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = memoized(n-1, dp) + memoized(n-2, dp);
-    }
-    public int tabulation(int n, int[] dp) {
-        // bottom-up approach
-        dp[0] = 1;
-        dp[1] = dp[0];
-        for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
-    }
-    public int spaceOptimized(int n) {
-        int prev = 1;
-        int prev2 = 1;
-
-        for(int i = 2; i <= n; i++) {
-            int cur = prev + prev2;
-            prev2 = prev;
-            prev = cur;
-        }
-        return prev;
+    public int solve(int currentStep, int n, int[] dp) {
+        if(currentStep > n) return 0;
+        if(currentStep == n) return 1;
+        if(dp[currentStep] != -1) return dp[currentStep];
+        return dp[currentStep] = solve(currentStep + 1, n, dp) + solve(currentStep + 2, n, dp);
     }
     public int climbStairs(int n) {
-        // return recursive(n); // recursive approach will give TLE
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-        // return memoized(n, dp);
-        // return tabulation(n, dp);
-        return spaceOptimized(n);
+        return solve(0, n, dp);
     }
 }
