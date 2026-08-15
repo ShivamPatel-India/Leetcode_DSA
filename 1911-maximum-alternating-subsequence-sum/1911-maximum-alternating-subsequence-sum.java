9d859df41dd8b9b2ccc85aeb0e08d5356c0885ce
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-15 04:00:37 -0700

    Time: 60 ms (38.61%), Space: 120.5 MB (45.01%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,13 +1,18 @@
 class Solution {
+    private int n;
+    private long[][] dp;
+    private long solve(int[] nums, int idx, int flag) {
+        if(idx >= n) return 0;
+        if(dp[idx][flag] != -1) return dp[idx][flag];
+        long val = (flag == 1) ? nums[idx] : -nums[idx];
+        long take = val + solve(nums, idx + 1, 1-flag);
+        long notTake = solve(nums, idx + 1, flag);
+        return dp[idx][flag] = Math.max(take, notTake);
+    }
     public long maxAlternatingSum(int[] nums) {
-        int n = nums.length;
-        long[][] t = new long[n+1][2];
-        for(int i = 1; i < n+1; i++) { // 1 : even, 0 : odd
-            // even
-            t[i][1] = Math.max(t[i-1][0] - nums[i-1], t[i-1][1]);
-            // odd
-            t[i][0] = Math.max(t[i-1][1] + nums[i-1], t[i-1][0]);
-        }
-        return Math.max(t[n][0], t[n][1]);
+        n = nums.length;
+        dp = new long[n][2];
+        for(long[] row: dp) Arrays.fill(row, -1);
+        return solve(nums, 0, 1);
     }
 }
