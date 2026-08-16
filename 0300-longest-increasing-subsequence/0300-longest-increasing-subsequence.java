089109088ea2a23788f83475227fb02faa390082
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-16 02:24:05 -0700

    Time: 419 ms (5.36%), Space: 236.8 MB (21.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,25 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        List<Integer> sorted = new ArrayList<>();
-        sorted.add(nums[0]);
-        for(int i = 1; i < n; i++) {
-            if(nums[i] > sorted.get(sorted.size()-1)) sorted.add(nums[i]);
-            else {
-                int index = lowerBound(sorted, nums[i]);
-                sorted.set(index, nums[i]);
-            }
+    private int n;
+    private int[][] dp;
+    public int solve(int[] nums, int i, int prev) {
+        if(i >= n) return 0;
+        if(prev != -1 && dp[i][prev] != -1) return dp[i][prev];
+
+        int take = 0;
+        if(prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + solve(nums, i+1, i);
         }
-        return sorted.size();
+        int skip = solve(nums, i+1, prev);
+        if(prev != -1) {
+            dp[i][prev] = Math.max(take, skip);
+        }
+        return Math.max(take, skip);
     }
-    private int lowerBound(List<Integer> sorted, int target) {
-        int left = 0;
-        int right = sorted.size();
-        int result = sorted.size();
-
-        while(left < right) {
-            int mid = left + (right - left) / 2;
-
-            // we need to find the smallest greater or equal element
-            if(sorted.get(mid) >= target) {
-                result = mid;
-                right = mid;
-            } else left = mid + 1;
-        }
-        return result;
+    public int lengthOfLIS(int[] nums) {
+        n = nums.length;
+        // state definition: dp[i][prev] would define the length of LIS in which the last element comes from "prev" index till the index i
+        dp = new int[n+1][n+1]; 
+        for(int[] row: dp) Arrays.fill(row, -1);
+        return solve(nums, 0, -1);
     }
 }
