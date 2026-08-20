d300e60a2376b11784db66c8afadc9863ee5a0bb
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-20 02:24:00 -0700

    Time: 133 ms (32.88%), Space: 49.4 MB (7.8%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -1,7 +1,10 @@
 class Solution {
+    // According to problme statement, we need to find the number of ways in which we can build the array of length n with each element value ranging from 1 to m (inclusive) and search_cost musk be k.
+
+    // for every position [0 to n-1] there are 1 to m possibilities.
     private int N, M, K;
-    private int MOD = 1_000_000_007;
-    private int[][][] dp;
+    private int MOD = 1_000_000_007;   
+    private int dp[][][] = new int[51][51][101];
     private int solve(int idx, int sc, int max) {
         if(idx == N) {
             if(sc == K) return 1;
@@ -10,19 +13,19 @@ class Solution {
         if(dp[idx][sc][max] != -1) return dp[idx][sc][max];
         int result = 0;
         for(int i = 1; i <= M; i++) {
-            if(i > max) result = (result + solve(idx+1, sc+1, i)) % MOD;
-            else result = (result + solve(idx+1, sc, max)) % MOD;
+            if(i > max) result = (result + solve(idx + 1, sc + 1, i)) % MOD;
+            else result = (result + solve(idx + 1, sc, max)) % MOD;
         }
         return dp[idx][sc][max] = result % MOD;
     }
     public int numOfArrays(int n, int m, int k) {
         N = n; M = m; K = k;
-        dp = new int[N+1][N+1][M+1];
         for(int[][] arr: dp) {
-            for(int[] row: arr){
+            for(int[] row: arr) {
                 Arrays.fill(row, -1);
             }
         }
-        return solve(0, 0, 0); // solve(int idx, int searchCost, int maxSoFar)
+        return solve(0, 0, 0);
+
     }
 }
