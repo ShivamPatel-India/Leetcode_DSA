65ebb5bdb224c29b68abf3c60581c5c44509490e
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-23 00:43:13 -0700

    Time: 2 ms (89.58%), Space: 44.3 MB (40.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,34 +1,51 @@
 class Solution {
-    private void dfs(int i, int j, int[][] grid, int[][] time, int currTime) {
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 || currTime>=time[i][j]) return;
-        time[i][j] = currTime;
-        dfs(i+1, j, grid, time, currTime + 1);
-        dfs(i-1, j, grid, time, currTime + 1);
-        dfs(i, j+1, grid, time, currTime + 1);
-        dfs(i, j-1, grid, time, currTime + 1);
-    }
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
 
-        int[][] time = new int[m][n];
+        // traverse the grid and find all the rotten oranges and total orages as well.
+        int totalOranges = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] != 0) time[i][j] = Integer.MAX_VALUE;
+                if(grid[i][j] != 0) totalOranges++;
+                if(grid[i][j] == 2) {
+                    q.add(new int[]{i, j});
+                }
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 2) dfs(i, j, grid, time, 0);
+
+        // now that we have rotten oranges we can apply bfs at the point of rotten oranges and for every level of bfs we can increase the minute
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        // we have to take this exploredOrages variable so that we can check if it equals totalOranges, than its possible to rot all oranges
+        int exploredOranges = 0;
+        int minTime = 0;
+        while(!q.isEmpty()) {
+            // we have rotten orange at this point
+            int size = q.size();
+            exploredOranges += size;
+            for(int i = 0; i < size; i++) {
+                int[] point = q.poll();
+                for(int k = 0; k < 4; k++) {
+                    int nx = point[0] + dx[k];
+                    int ny = point[1] + dy[k];
+
+                    // we can ignore the new cell {nx, ny} if and only if
+                    // 1). cell is invalid
+                    // 2). orange in the cell already rotten
+                    // 3). cell is empty
+                    if(nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] == 2 || grid[nx][ny] == 0) continue;
+
+                    // if cell is valid then we can rott it
+                    grid[nx][ny] = 2;
+                    // after rotting it we have to put it in the queue so that we can rott its neighbors in the next pass
+                    q.add(new int[]{nx, ny});
+                }
             }
+            if(!q.isEmpty()) minTime++;
         }
-        int minTime = Integer.MIN_VALUE;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(time[i][j] == Integer.MAX_VALUE) return -1;
-                minTime = Math.max(minTime, time[i][j]);
-            }
-        }
-        return minTime;
+        return exploredOranges == totalOranges ? minTime : -1;
     }
 }
