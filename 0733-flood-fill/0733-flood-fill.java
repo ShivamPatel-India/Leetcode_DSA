991fcd143a755263a452f64b2813f7f31edd58fa
ShivamPatel <shivampatel.sp515@gmail.com>
2026-08-23 03:26:01 -0700

    Time: 0 ms (100%), Space: 46.2 MB (99.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,15 @@
 class Solution {
-    private int n;
     private int m;
-    private void dfs(int[][] image, int sr, int sc, int color, int originalColor) {
-        if(sr < 0 || sc < 0 || sr >= m || sc >= n || image[sr][sc] != originalColor || image[sr][sc] == color) return;
-        image[sr][sc] = color;
-        dfs(image, sr+1, sc, color, originalColor);
-        dfs(image, sr-1, sc, color, originalColor);
-        dfs(image, sr, sc+1, color, originalColor);
-        dfs(image, sr, sc-1, color, originalColor);
+    private int n;
+    private void dfs(int[][] image, int i, int j, int color, int originalColor) {
+        if(i < 0 || j < 0 || i >= m || j >= n || image[i][j] == color || image[i][j] != originalColor) return;
+        image[i][j] = color;
+        dfs(image, i+1, j, color, originalColor);
+        dfs(image, i-1, j, color, originalColor);
+        dfs(image, i, j+1, color, originalColor);
+        dfs(image, i, j-1, color, originalColor);
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // in this problem, we can just apply the bfs and repaint every cell
         m = image.length;
         n = image[0].length;
         int originalColor = image[sr][sc];
