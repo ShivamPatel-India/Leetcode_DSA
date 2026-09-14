d1c69e96a242298e787220ae34970bbf102433a1
ShivamPatel <shivampatel.sp515@gmail.com>
2026-09-13 17:12:00 -0700

    Time: 1 ms (38.03%), Space: 46.7 MB (61.32%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,30 @@
 class Solution {
-    private int m;
-    private int n;
-    private void dfs(int[][] image, int i, int j, int color, int originalColor) {
-        if(i < 0 || j < 0 || i >= m || j >= n || image[i][j] == color || image[i][j] != originalColor) return;
-        image[i][j] = color;
-        dfs(image, i+1, j, color, originalColor);
-        dfs(image, i-1, j, color, originalColor);
-        dfs(image, i, j+1, color, originalColor);
-        dfs(image, i, j-1, color, originalColor);
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        m = image.length;
-        n = image[0].length;
+        int m = image.length;
+        int n = image[0].length;
         int originalColor = image[sr][sc];
-        dfs(image, sr, sc, color, originalColor);
+        if(originalColor == color) return image;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{sr, sc});
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        while(!q.isEmpty()) {
+            int k = q.size();
+            for(int i = 0; i < k; i++) {
+                int[] pos = q.poll();
+                int x = pos[0];
+                int y = pos[1];
+                image[x][y] = color;
+                for(int j = 0; j < 4; j++) {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+                    if(nx<0 || ny<0 || nx>=m || ny>=n || image[nx][ny] != originalColor) continue;
+                    image[nx][ny] = color;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
         return image;
     }
 }
